8a3cc99c33c923170dc600f6605c7c24efb8c91a
yamadapc <tacla.yamada@gmail.com>
2021-06-01 07:59:24 +1000

    Fix bugs in test host

--- crates/test-plugin-host/src/main.rs
+++ crates/test-plugin-host/src/main.rs
@@ -1,21 +1,27 @@
+use std::process::exit;
+
 mod commands;
 mod host;
 
 fn main() {
     wisual_logger::init_from_env();
-    let matches = clap::App::new("test-plugin-host")
+    let mut app = clap::App::new("test-plugin-host")
         .version("0.0.1")
         .author("Pedro Tacla Yamada <tacla.yamada@gmail.com>")
         .about("Test audio plugins")
         .subcommand(commands::options::build_run_command())
-        .subcommand(clap::App::new("list-devices").about("Lists audio devices"))
-        .get_matches();
+        .subcommand(clap::App::new("list-devices").about("Lists audio devices"));
+    let matches = app.clone().get_matches();
 
     if matches.is_present("list-devices") {
         commands::run_list_devices();
         return;
     }
 
-    let run_options = commands::options::parse_run_options(matches).unwrap();
-    commands::run_test(run_options);
+    if let Some(run_options) = commands::options::parse_run_options(matches) {
+        commands::run_test(run_options);
+    } else {
+        app.print_help().unwrap();
+        exit(1);
+    }
 }
