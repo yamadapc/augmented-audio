28e6594d03c1eb55a17380f7b75bde8aa302f815
yamadapc <tacla.yamada@gmail.com>
2021-06-01 07:34:58 +1000

    Update test script

--- scripts/build-and-test-rust.sh
+++ scripts/build-and-test-rust.sh
@@ -3,13 +3,7 @@
 set -x
 set -e
 
-for crate in ./crates/*; do
-  if [ -d "$crate" ]; then
-    cd "$crate"
-    cargo build
-    cargo test
-    cargo fmt
-    cargo clippy
-    cd -
-  fi
-done
+cargo build
+cargo test
+cargo fmt
+cargo clippy
