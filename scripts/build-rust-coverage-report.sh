aec7842b131ed8f75067bb0f200d05dd0f34b734
yamadapc <tacla.yamada@gmail.com>
2021-06-09 18:50:05 +1000

    Update build scripts

--- scripts/build-rust-coverage-report.sh
+++ scripts/build-rust-coverage-report.sh
@@ -5,12 +5,10 @@ set -x
 
 rm -f default.profraw
 
-RUSTFLAGS="-Zinstrument-coverage" cargo +nightly build
 RUSTFLAGS="-Zinstrument-coverage" cargo +nightly test
 
 grcov . \
   --binary-path ./target/debug \
   --source-dir . \
   -t html --branch \
-  --ignore-not-existing \
   -o "./coverage/"
