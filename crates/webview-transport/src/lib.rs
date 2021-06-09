b6c35546e5aa739460892458fbb445f8b52fe62a
yamadapc <tacla.yamada@gmail.com>
2021-06-09 19:17:08 +1000

    Conditionally compile webkit transport based on OS

--- crates/webview-transport/src/lib.rs
+++ crates/webview-transport/src/lib.rs
@@ -3,11 +3,13 @@ extern crate async_trait;
 
 pub mod delegating;
 pub mod transport;
+#[cfg(any(target_os = "ios", target_os = "macos"))]
 pub mod webkit;
 pub mod websockets;
 
 pub use delegating::DelegatingTransport;
 pub use transport::WebviewTransport;
+#[cfg(any(target_os = "ios", target_os = "macos"))]
 pub use webkit::WebkitTransport;
 pub use websockets::{create_transport_runtime, WebSocketsTransport};
 
