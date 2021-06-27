f773ef9c4d65a0369f6b70efe154852f86cee6c4
yamadapc <tacla.yamada@gmail.com>
2021-06-28 07:15:15 +1000

    Remove unused tests

--- crates/webview-transport/src/lib.rs
+++ crates/webview-transport/src/lib.rs
@@ -1,23 +1,15 @@
 #[macro_use]
 extern crate async_trait;
 
-pub mod channel;
-pub mod delegating;
-pub mod transport;
-#[cfg(any(target_os = "ios", target_os = "macos"))]
-pub mod webkit;
-pub mod websockets;
-
 pub use delegating::DelegatingTransport;
 pub use transport::WebviewTransport;
 #[cfg(any(target_os = "ios", target_os = "macos"))]
 pub use webkit::WebkitTransport;
 pub use websockets::{create_transport_runtime, WebSocketsTransport};
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_compiles() {
-        assert_eq!(4, 4);
-    }
-}
+pub mod channel;
+pub mod delegating;
+pub mod transport;
+#[cfg(any(target_os = "ios", target_os = "macos"))]
+pub mod webkit;
+pub mod websockets;
