4a07490ec6879d760b0e917f0a492c9e282631c9
yamadapc <tacla.yamada@gmail.com>
2021-06-27 16:53:03 +1000

    Add more basic benchmarks

--- crates/oscillator/benches/sine_oscillator_benchmark.rs
+++ crates/oscillator/benches/sine_oscillator_benchmark.rs
@@ -1,4 +1,5 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use oscillator::generators::{saw_generator, square_generator};
 use oscillator::Oscillator;
 
 fn sine_osc(oscillator: &mut Oscillator<f32>, output_buffer: &mut [f32]) {
@@ -8,11 +9,29 @@ fn sine_osc(oscillator: &mut Oscillator<f32>, output_buffer: &mut [f32]) {
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("sine_osc - 1000 samples", |b| {
+    c.bench_function("sine_osc - 10k samples", |b| {
         let mut oscillator = Oscillator::sine(44100.0);
         oscillator.set_frequency(440.0);
         let mut output_buffer = Vec::new();
-        output_buffer.resize(1000, 0.0);
+        output_buffer.resize(10000, 0.0);
+
+        b.iter(|| sine_osc(&mut oscillator, black_box(&mut output_buffer)))
+    });
+
+    c.bench_function("saw_osc - 10k samples", |b| {
+        let mut oscillator = Oscillator::new_with_sample_rate(44100.0, saw_generator);
+        oscillator.set_frequency(440.0);
+        let mut output_buffer = Vec::new();
+        output_buffer.resize(10000, 0.0);
+
+        b.iter(|| sine_osc(&mut oscillator, black_box(&mut output_buffer)))
+    });
+
+    c.bench_function("square_osc - 10k samples", |b| {
+        let mut oscillator = Oscillator::new_with_sample_rate(44100.0, square_generator);
+        oscillator.set_frequency(440.0);
+        let mut output_buffer = Vec::new();
+        output_buffer.resize(10000, 0.0);
 
         b.iter(|| sine_osc(&mut oscillator, black_box(&mut output_buffer)))
     });
