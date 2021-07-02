f905385bc181f2c062aaf815bf5e0b49f2feaec6
yamadapc <tacla.yamada@gmail.com>
2021-07-03 08:06:19 +1000

    Start thinking about AudioBuffer idea

--- crates/audio-processor-traits/src/lib.rs
+++ crates/audio-processor-traits/src/lib.rs
@@ -64,3 +64,59 @@ impl AudioProcessor for SilenceAudioProcessor {
         }
     }
 }
+
+/// Passing around `&mut [f32]` as audio buffers isn't good because:
+///
+/// * Some libraries / APIs will use interleaved buffers
+/// * Some will not
+/// * If you pick one all your processor code is bound to a buffer layout
+/// * If there's an abstraction on top the processor code can work for any buffer layout while
+///   still having the sample performance
+/// * Currently `AudioProcessor` is made to work with cpal interleaved buffers; it then needs
+///   conversion to work with VST.
+/// * That's very unfortunate. I'd like to write a single processor that can work with both buffer
+///   types with no overhead.
+pub trait AudioBuffer<SampleType> {
+    fn num_channels(&self) -> usize;
+    fn num_samples(&self) -> usize;
+    fn get(&self, channel: usize, sample: usize) -> &SampleType;
+    fn get_mut(&mut self, channel: usize, sample: usize) -> &mut SampleType;
+    fn set(&mut self, channel: usize, sample: usize, value: SampleType);
+}
+
+pub struct InterleavedAudioBuffer<'a, SampleType> {
+    num_channels: usize,
+    inner: &'a mut [SampleType],
+}
+
+impl<'a, SampleType> InterleavedAudioBuffer<'a, SampleType> {
+    pub fn new(num_channels: usize, inner: &'a mut [SampleType]) -> Self {
+        Self {
+            num_channels,
+            inner,
+        }
+    }
+}
+
+impl<'a, SampleType> AudioBuffer<SampleType> for InterleavedAudioBuffer<'a, SampleType> {
+    fn num_channels(&self) -> usize {
+        self.num_channels
+    }
+
+    fn num_samples(&self) -> usize {
+        self.inner.len() / self.num_channels
+    }
+
+    fn get(&self, channel: usize, sample: usize) -> &SampleType {
+        &self.inner[sample * self.num_channels + channel]
+    }
+
+    fn get_mut(&mut self, channel: usize, sample: usize) -> &mut SampleType {
+        &mut self.inner[sample * self.num_channels + channel]
+    }
+
+    fn set(&mut self, channel: usize, sample: usize, value: SampleType) {
+        let sample_ref = self.get_mut(channel, sample);
+        *sample_ref = value;
+    }
+}
