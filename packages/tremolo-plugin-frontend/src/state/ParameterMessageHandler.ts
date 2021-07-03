0fcfbe178f3aa9c046c67b0370b04eef870ca396
yamadapc <tacla.yamada@gmail.com>
2021-07-04 07:59:56 +1000

    Fix formatting

--- packages/tremolo-plugin-frontend/src/state/ParameterMessageHandler.ts
+++ packages/tremolo-plugin-frontend/src/state/ParameterMessageHandler.ts
@@ -1,8 +1,8 @@
-import {singleton} from "tsyringe";
-import {MessageHandler} from "./MessageHandler";
-import {ParametersStore} from "./ParametersStore";
-import {LoggerFactory} from "@wisual/logger";
-import {ClientMessageInner, ServerMessage} from "../common/protocol";
+import { singleton } from "tsyringe";
+import { MessageHandler } from "./MessageHandler";
+import { ParametersStore } from "./ParametersStore";
+import { LoggerFactory } from "@wisual/logger";
+import { ClientMessageInner, ServerMessage } from "../common/protocol";
 
 @singleton()
 export class ParameterMessageHandler implements MessageHandler {
