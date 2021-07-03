ea1cba288989ae3fe2dc255ef8127d8d02705b02
yamadapc <tacla.yamada@gmail.com>
2021-07-04 09:08:39 +1000

    Move files around

--- packages/looper-plugin-frontend/src/index.tsx
+++ packages/looper-plugin-frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './ui/index.css';
+import App from './ui/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
