b9b3d079d90827a0493b549d5e9506a02a132c3f
yamadapc <tacla.yamada@gmail.com>
2021-07-06 17:49:08 +1000

    Add fallback bundle resources code

--- crates/macos-bundle-resources/src/lib.rs
+++ crates/macos-bundle-resources/src/lib.rs
@@ -1,5 +1,25 @@
+#[cfg(not(target_os = "macos"))]
+pub use fallback::*;
 #[cfg(target_os = "macos")]
 pub use macos::*;
 
 #[cfg(target_os = "macos")]
 mod macos;
+
+#[cfg(not(target_os = "macos"))]
+mod fallback {
+    use std::path::PathBuf;
+
+    pub fn has_bundle(_bundle_identifier: &str) -> bool {
+        false
+    }
+
+    pub fn get_path(
+        _bundle_identifier: &str,
+        _resource_name: &str,
+        _resource_type: Option<&str>,
+        _sub_dir_name: Option<&str>,
+    ) -> Option<PathBuf> {
+        None
+    }
+}
