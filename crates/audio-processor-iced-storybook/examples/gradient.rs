24ca69702a4aabd889eae8ced97de41f7b7d0bff
yamadapc <tacla.yamada@gmail.com>
2021-07-17 09:47:25 +1000

    Fix copy-paste problem rendering multiple gradients :P

--- crates/audio-processor-iced-storybook/examples/gradient.rs
+++ crates/audio-processor-iced-storybook/examples/gradient.rs
@@ -1,16 +1,69 @@
 use iced::container::Style;
 use iced::{
-    container, Background, Color, Container, Direction, Gradient, GradientStop, LinearGradient,
-    Text,
+    container, Background, Color, Container, Direction, Gradient, GradientStop, Length,
+    LinearGradient, Row, Text,
 };
 
 fn main() -> iced::Result {
     audio_processor_iced_storybook::builder::<()>()
         .story_fn("Gradient", || {
-            Container::new(Text::new("Hey"))
-                .style(ContainerStyle)
-                .padding(50)
-                .into()
+            Row::with_children(vec![
+                Container::new(Text::new("Hey"))
+                    .style(ContainerStyle)
+                    .padding(50)
+                    .into(),
+                Container::new(Text::new("Hey"))
+                    .style(ContainerStyle)
+                    .padding(50)
+                    .into(),
+                Container::new(Text::new("Hey"))
+                    .style(ContainerStyle)
+                    .padding(50)
+                    .into(),
+                Container::new(Text::new("Hey"))
+                    .style(ContainerStyle)
+                    .padding(50)
+                    .into(),
+                Container::new(Text::new("Hey"))
+                    .style(ContainerStyle)
+                    .padding(50)
+                    .into(),
+            ])
+            .height(Length::Fill)
+            .width(Length::Fill)
+            .into()
+        })
+        .story_fn("Gradient 2 steps", || {
+            Row::with_children(vec![
+                Container::new(Text::new("Hey"))
+                    .style(Container2StepsStyle)
+                    .padding(50)
+                    .height(Length::Fill)
+                    .into(),
+                Container::new(Text::new("Hey"))
+                    .style(Container2StepsStyle)
+                    .padding(50)
+                    .height(Length::Fill)
+                    .into(),
+                Container::new(Text::new("Hey"))
+                    .style(Container2StepsStyle)
+                    .padding(50)
+                    .height(Length::Fill)
+                    .into(),
+                Container::new(Text::new("Hey"))
+                    .style(Container2StepsStyle)
+                    .padding(50)
+                    .height(Length::Fill)
+                    .into(),
+                Container::new(Text::new("Hey"))
+                    .style(Container2StepsStyle)
+                    .padding(50)
+                    .height(Length::Fill)
+                    .into(),
+            ])
+            .height(Length::Fill)
+            .width(Length::Fill)
+            .into()
         })
         .run()
 }
@@ -32,6 +85,38 @@ impl container::StyleSheet for ContainerStyle {
                         GradientStop {
                             percentage: 1.0,
                             color: Color::from_rgb(0.0, 0.0, 1.0),
+                        },
+                    ],
+                },
+            ))),
+            border_radius: 0.0,
+            border_width: 0.0,
+            border_color: Default::default(),
+        }
+    }
+}
+
+struct Container2StepsStyle;
+
+impl container::StyleSheet for Container2StepsStyle {
+    fn style(&self) -> Style {
+        Style {
+            text_color: None,
+            background: Some(Background::Gradient(Gradient::LinearGradient(
+                LinearGradient {
+                    direction: Direction::Top,
+                    stops: vec![
+                        GradientStop {
+                            percentage: 0.0,
+                            color: Color::from_rgb(1.0, 0.0, 0.0),
+                        },
+                        GradientStop {
+                            percentage: 0.5,
+                            color: Color::from_rgb(0.0, 0.0, 1.0),
+                        },
+                        GradientStop {
+                            percentage: 1.0,
+                            color: Color::from_rgb(0.0, 1.0, 0.0),
                         },
                     ],
                 },
