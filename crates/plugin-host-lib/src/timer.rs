de5aacb2a7c1bbf8bd472670100c9cbf43076877
yamadapc <tacla.yamada@gmail.com>
2021-07-20 07:33:10 +1000

    Add unit-tests for time

--- crates/plugin-host-lib/src/timer.rs
+++ crates/plugin-host-lib/src/timer.rs
@@ -6,3 +6,14 @@ pub fn time<T>(label: &str, body: impl FnOnce() -> T) -> T {
     log::info!("{} duration={}ms", label, start.elapsed().as_millis());
     result
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_time() {
+        let result = time("test_time", || 10_i32.pow(2));
+        assert_eq!(result, 100);
+    }
+}
