87914cfcbc5d7345b0a735a12c7cfd7b0c737dc9
yamadapc <tacla.yamada@gmail.com>
2021-07-23 07:31:22 +1000

    Update code-coverage script #27

--- scripts/build-rust-coverage-report.sh
+++ scripts/build-rust-coverage-report.sh
@@ -4,6 +4,10 @@ set -e
 set -x
 
 rm -f default.profraw
+rm -f ./*.profraw
+rm -f ./crates/**/*.profraw
+
+rm -rf ./coverage/*
 
 RUSTFLAGS="-Zinstrument-coverage" cargo +nightly build --target-dir=./target-nightly
 
@@ -13,10 +17,13 @@ grcov . \
   --binary-path ./target-nightly/debug \
   --source-dir . \
   -t html --branch \
-  --ignore "./packages/*" \
-  --ignore "./crates/baseview" \
-  --ignore "./crates/cpal" \
-  --ignore "./crates/iced" \
-  --ignore "./crates/iced_audio" \
-  --ignore "./crates/plotters" \
-  -o "./coverage/"
+  --ignore ./packages/* \
+  --ignore ./crates/baseview \
+  --ignore ./crates/cpal \
+  --ignore ./crates/iced \
+  --ignore ./crates/iced_audio \
+  --ignore ./crates/plotters \
+  -o ./coverage/
+
+rm -f ./*.profraw
+rm -f ./crates/**/*.profraw
