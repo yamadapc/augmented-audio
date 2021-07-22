42556d64bd7826f2d0076fabd26c41b2712542e3
yamadapc <tacla.yamada@gmail.com>
2021-07-22 17:33:24 +1000

    Minor project updates

--- scripts/build-rust-coverage-report.sh
+++ scripts/build-rust-coverage-report.sh
@@ -5,10 +5,18 @@ set -x
 
 rm -f default.profraw
 
-RUSTFLAGS="-Zinstrument-coverage" cargo +nightly test --target-dir=./target-nightly
+RUSTFLAGS="-Zinstrument-coverage" cargo +nightly build --target-dir=./target-nightly
+
+LLVM_PROFILE_FILE="augmented_audio-%p-%m.profraw" RUSTFLAGS="-Zinstrument-coverage" cargo +nightly test --target-dir=./target-nightly
 
 grcov . \
   --binary-path ./target-nightly/debug \
   --source-dir . \
   -t html --branch \
+  --ignore "./packages/*" \
+  --ignore "./crates/baseview" \
+  --ignore "./crates/cpal" \
+  --ignore "./crates/iced" \
+  --ignore "./crates/iced_audio" \
+  --ignore "./crates/plotters" \
   -o "./coverage/"
