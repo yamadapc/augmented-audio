fe26436f64ff6cc1d7787e5d49fabdd674f29f8f
yamadapc <tacla.yamada@gmail.com>
2021-08-19 07:34:14 +1000

    Try to fix clippy running over generated code

--- crates/augmented/audio/audiounit/src/macos/sys.rs
+++ crates/augmented/audio/audiounit/src/macos/sys.rs
@@ -3,13 +3,21 @@ mod test {
     use cocoa::base::YES;
 
     use avfaudio_sys::{
-        AVAudioUnitComponentManager, IAVAudioUnitComponentManager, INSPredicate, NSPredicate,
+        AVAudioUnitComponent, AVAudioUnitComponentManager, IAVAudioUnitComponent,
+        IAVAudioUnitComponentManager, INSArray, INSPredicate, NSPredicate,
     };
 
     #[test]
     fn list_all_audio_units() {
-        let manager = AVAudioUnitComponentManager::sharedAudioUnitComponentManager();
-        let components =
-            manager.componentsMatchingPredicate_(NSPredicate::predicateWithValue_(YES));
+        unsafe {
+            let manager: AVAudioUnitComponentManager = AVAudioUnitComponentManager(
+                AVAudioUnitComponentManager::sharedAudioUnitComponentManager(),
+            );
+            let components =
+                manager.componentsMatchingPredicate_(NSPredicate::predicateWithValue_(YES));
+            for i in 0..INSArray::<AVAudioUnitComponent>::count(components) {
+                let component: AVAudioUnitComponent = components.objectAtIndex_(i);
+            }
+        }
     }
 }
