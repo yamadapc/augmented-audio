03cb7a87b6a88ca6efbc2b8aca5115408d095aef
yamadapc <tacla.yamada@gmail.com>
2021-08-27 16:51:13 +1000

    Get build infrastructure working for rec. buddy

---
+++ crates/apps/recording_buddy/Recording Buddy/Recording_Buddy-Bridging-Header.h
@@ -0,0 +1,7 @@
+#ifndef Recording_Buddy_Bridging_Header_h
+#define Recording_Buddy_Bridging_Header_h
+
+#include "../src/Generated/augmentedFFI.h"
+#include "../src/Generated/bindings.h"
+
+#endif
--- crates/apps/recording_buddy/src/lib.rs
+++ crates/apps/recording_buddy/src/lib.rs
@@ -1,3 +1,79 @@
-fn hello_world() {
-    println!("Hello, world!");
+use std::thread;
+use std::time::Duration;
+
+use callbacks::*;
+use plugin_host_lib::audio_io::AudioIOService;
+
+// Taken from - https://www.nickwilcox.com/blog/recipe_swift_rust_callback/
+mod callbacks {
+    use std::ffi::c_void;
+
+    #[repr(C)]
+    pub struct CompletedCallback {
+        userdata: *mut c_void,
+        callback: extern "C" fn(*mut c_void, bool),
+    }
+
+    unsafe impl Send for CompletedCallback {}
+
+    impl CompletedCallback {
+        pub fn succeeded(self) {
+            (self.callback)(self.userdata, true);
+            std::mem::forget(self)
+        }
+        pub fn failed(self) {
+            (self.callback)(self.userdata, false);
+            std::mem::forget(self)
+        }
+    }
+
+    impl Drop for CompletedCallback {
+        fn drop(&mut self) {
+            panic!("CompletedCallback must have explicit succeeded or failed call")
+        }
+    }
 }
+
+#[no_mangle]
+pub extern "C" fn async_operation(callback: CompletedCallback) {
+    thread::spawn(move || {
+        thread::sleep(Duration::from_secs(3));
+        callback.succeeded()
+    });
+}
+
+pub fn initialize_logger() {
+    let _ = wisual_logger::try_init_from_env();
+}
+
+pub struct AudioGuiInitialModel {
+    host_ids: Vec<String>,
+    input_ids: Vec<String>,
+    output_ids: Vec<String>,
+}
+
+pub fn get_audio_info() -> AudioGuiInitialModel {
+    log::info!("get_audio_info called");
+    let host_list = AudioIOService::hosts();
+    let input_list = AudioIOService::input_devices(None).unwrap();
+    let output_list = AudioIOService::output_devices(None).unwrap();
+
+    AudioGuiInitialModel {
+        host_ids: host_list,
+        input_ids: input_list.into_iter().map(|device| device.name).collect(),
+        output_ids: output_list.into_iter().map(|device| device.name).collect(),
+    }
+}
+
+#[derive(Debug)]
+pub struct AudioGuiModel {
+    host_id: Option<String>,
+    input_id: Option<String>,
+    output_id: Option<String>,
+}
+
+pub fn set_audio_info(model: AudioGuiModel) {
+    log::info!("set_audio_info called with {:?}", model);
+}
+
+uniffi_macros::include_scaffolding!("augmented");
