dbdf261f3276f9a08664749e4492f3a1bc8e0878
yamadapc <tacla.yamada@gmail.com>
2021-10-30 08:49:50 +1100

    Ignore more rustc warnings on generated bindings

--- crates/augmented/audio/avfaudio-sys/src/lib.rs
+++ crates/augmented/audio/avfaudio-sys/src/lib.rs
@@ -3,7 +3,9 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+// These lints ignore unsafe undefined behaviour
 #![allow(deref_nullptr)]
 #![allow(improper_ctypes)]
+#![allow(unaligned_references)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
