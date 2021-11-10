9d5e41484324434cb46f4637b5a498187e871589
yamadapc <tacla.yamada@gmail.com>
2021-11-10 20:54:10 +1100

    Move on with qt spike

--- crates/apps/plugin-host/plugin-host-gui3/src/MainWindow.cpp
+++ crates/apps/plugin-host/plugin-host-gui3/src/MainWindow.cpp
@@ -15,17 +15,27 @@ MainWindow::MainWindow (QWidget* parent) : QMainWindow (parent), ui (new Ui::Mai
     leftDockWidget->setAllowedAreas (Qt::LeftDockWidgetArea);
     addDockWidget (Qt::LeftDockWidgetArea, leftDockWidget);
     leftDockWidget->setStyleSheet ("QDockWidget { min-width: 200px; }");
+    leftDockWidget->setFeatures (QDockWidget::NoDockWidgetFeatures);
+    leftDockWidget->setTitleBarWidget (new QWidget (this));
+
+    auto* leftDockContents = new QWidget(this);
+    auto* button = new QPushButton ("Hello world", leftDockContents);
+    leftDockWidget->setWidget (leftDockContents);
 
     contentsWindow = new QWidget (this);
+    contentsWindow->setBackgroundRole (QPalette::Window);
     contentsWindow->setAutoFillBackground (true);
     button = new QPushButton ("Click me", contentsWindow);
-    button->setStyleSheet("QPushButton::pressed { background-color: blue; }");
+    // button->setStyleSheet("QPushButton::pressed { background-color: blue; }");
 
     setCentralWidget (contentsWindow);
 
     setStyleSheet ("QMainWindow { min-width: 500px; min-height: 500px; }"
                    "QMainWindow::separator { background: rgb(180, 180, 180); width: 1px; height: 1px; }");
+
     resize (800, 500);
+    this->setBackgroundRole (QPalette::Window);
+    this->setAutoFillBackground (true);
 }
 
 MainWindow::~MainWindow ()
