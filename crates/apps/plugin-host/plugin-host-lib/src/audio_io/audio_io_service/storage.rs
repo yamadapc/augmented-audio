b79423417c0b1c5781fbc67e748b9a4cb0b3c3f6
yamadapc <tacla.yamada@gmail.com>
2021-11-11 18:20:04 +1100

    Minor updates using actors

--- crates/apps/plugin-host/plugin-host-lib/src/audio_io/audio_io_service/storage.rs
+++ crates/apps/plugin-host/plugin-host-lib/src/audio_io/audio_io_service/storage.rs
@@ -1,3 +1,7 @@
+use actix::Actor;
+use actix::Context;
+use actix::Message;
+use actix_handler_macro::actix_handler;
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
@@ -38,20 +42,48 @@ impl AudioIOStorageService {
     }
 }
 
+impl Actor for AudioIOStorageService {
+    type Context = Context<Self>;
+}
+
+#[derive(Message)]
+#[rtype(result = "Result<AudioIOState, AudioIOStorageServiceError>")]
+pub struct FetchMessage;
+
+#[derive(Message)]
+#[rtype(result = "Result<(), AudioIOStorageServiceError>")]
+pub struct StoreMessage {
+    state: AudioIOState,
+}
+
+#[actix_handler]
+impl AudioIOStorageService {
+    fn handle_fetch(
+        &self,
+        _message: FetchMessage,
+        _ctx: &mut Context<Self>,
+    ) -> Result<AudioIOState, AudioIOStorageServiceError> {
+        self.fetch()
+    }
+
+    fn handle_store(
+        &self,
+        message: StoreMessage,
+        _ctx: &mut Context<Self>,
+    ) -> Result<(), AudioIOStorageServiceError> {
+        self.store(&message.state)
+    }
+}
+
 #[cfg(test)]
 mod test {
+    use std::path::PathBuf;
+
     use super::*;
 
     #[test]
     fn test_store_file() {
-        let tmp_dir = std::env::temp_dir();
-        std::fs::create_dir_all(&tmp_dir).unwrap();
-        let tmp_file_path = tmp_dir.join("plugin-host-lib-test-store-file.json");
-        let _ = std::fs::remove_file(&tmp_file_path);
-
-        let config = StorageConfig {
-            audio_io_state_storage_path: String::from(tmp_file_path.to_str().unwrap()),
-        };
+        let (tmp_file_path, config) = build_tmp_config();
         let service = AudioIOStorageService::new(config);
 
         let state = AudioIOState {
@@ -64,5 +96,40 @@ mod test {
         assert_eq!(stored_state, state);
 
         std::fs::remove_file(&tmp_file_path).unwrap();
+    }
+
+    #[actix::test]
+    async fn test_actix_spawn() {
+        let (tmp_file_path, config) = build_tmp_config();
+        let service = AudioIOStorageService::new(config);
+        let service = service.start();
+        let state = AudioIOState {
+            host: "Some host".to_string(),
+            input_device: None,
+            output_device: None,
+        };
+        service
+            .send(StoreMessage {
+                state: state.clone(),
+            })
+            .await
+            .unwrap();
+        let stored_state = service.send(FetchMessage).await.unwrap().unwrap();
+        assert_eq!(stored_state, state);
+
+        std::fs::remove_file(&tmp_file_path).unwrap();
+    }
+
+    fn build_tmp_config() -> (PathBuf, StorageConfig) {
+        let id = uuid::Uuid::new_v4().to_string();
+        let tmp_dir = std::env::temp_dir();
+        std::fs::create_dir_all(&tmp_dir).unwrap();
+        let tmp_file_path = tmp_dir.join(format!("plugin-host-lib-test-store-file__{}.json", id));
+        let _ = std::fs::remove_file(&tmp_file_path);
+
+        let config = StorageConfig {
+            audio_io_state_storage_path: String::from(tmp_file_path.to_str().unwrap()),
+        };
+        (tmp_file_path, config)
     }
 }
--- crates/spikes/example-druid-audio-viz/src/main.rs
+++ crates/spikes/example-druid-audio-viz/src/main.rs
@@ -1,17 +1,3 @@
-// Copyright 2020 The Druid Authors.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 //! An example of sending commands from another thread.
 //! This is useful when you want to have some kind of
 //! generated content (like here), or some task that just
@@ -44,10 +30,6 @@ pub fn main() {
     let window = WindowDesc::new(|| make_ui()).title("External Event Demo");
 
     let launcher = AppLauncher::with_window(window);
-
-    // If we want to create commands from another thread `launcher.get_external_handle()`
-    // should be used. For sending commands from within widgets you can always call
-    // `ctx.submit_command`
     let event_sink = launcher.get_external_handle();
 
     let garbage_collector = GarbageCollector::default();
