bcc46e115333980da23e3d2d10a667d820f89db8
yamadapc <tacla.yamada@gmail.com>
2021-11-21 10:47:17 +1100

    Update with golden test dependency

--- lib/ui/main_content_layout/bottom_panel.dart
+++ lib/ui/main_content_layout/bottom_panel.dart
@@ -4,12 +4,13 @@ import 'package:flutter_daw_mock_ui/ui/common/tabs.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:mobx/mobx.dart';
 
+import 'bottom_panel/track_effects.dart';
 import 'midi_editor/midi_editor.dart';
 
 Observable<double> heightObservable = Observable(200);
 
 class BottomPanel extends StatelessWidget {
-  BottomPanel({Key? key}) : super(key: key);
+  const BottomPanel({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +23,7 @@ class BottomPanel extends StatelessWidget {
                 child: MouseRegion(
                   cursor: SystemMouseCursors.resizeUpDown,
                   child: Container(
-                      decoration: BoxDecoration(color: Colors.red),
+                      decoration: const BoxDecoration(color: Colors.red),
                       height: 5,
                       width: double.infinity),
                 )),
@@ -53,54 +54,5 @@ class BottomPanel extends StatelessWidget {
     runInAction(() {
       heightObservable.value -= details.delta.dy;
     });
-  }
-}
-
-class TrackEffectsView extends StatelessWidget {
-  const TrackEffectsView({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(children: const [
-      SizedBox(width: 300, child: AudioEffectView()),
-      SizedBox(width: 300, child: AudioEffectView()),
-      SizedBox(width: 300, child: AudioEffectView()),
-    ]);
-  }
-}
-
-class AudioEffectView extends StatelessWidget {
-  const AudioEffectView({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        margin: const EdgeInsets.all(8.0),
-        decoration: BoxDecoration(
-            color: const Color.fromRGBO(80, 80, 80, 1.0),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.4),
-                spreadRadius: 1.0,
-                blurRadius: 5.0,
-              )
-            ],
-            border: Border.all(color: const Color.fromRGBO(65, 65, 65, 1.0))),
-        child: Column(children: [
-          Container(
-              height: 20,
-              padding: const EdgeInsets.only(left: 4, right: 4),
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                color: Color.fromRGBO(130, 130, 130, 1.0),
-              ),
-              child: const DefaultTextStyle(
-                  style: TextStyle(color: Colors.black), child: Text("Delay"))),
-          Expanded(
-            child: Container(child: null),
-          ),
-        ]));
   }
 }
