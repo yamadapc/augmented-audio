ae7b3f2e11394cdeb350ede0c0d5bef9588bed93
yamadapc <tacla.yamada@gmail.com>
2021-11-22 15:27:52 +1100

    Disable debug sandbox & fix problems

--- crates/apps/daw/lib/state/wire.dart
+++ crates/apps/daw/lib/state/wire.dart
@@ -3,6 +3,7 @@ import 'dart:ffi';
 import 'package:flutter_daw_mock_ui/bridge_generated.dart';
 
 FlutterDawMockUi initialize() {
-  var dylib = DynamicLibrary.open("../../../target/debug/libdaw_ui.dylib")
+  var dylib = DynamicLibrary.open(
+      "/Users/yamadapc/projects/rust-audio-software/target/debug/libdaw_ui.dylib");
   return FlutterDawMockUi(dylib);
 }
