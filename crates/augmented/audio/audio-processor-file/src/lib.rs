6d4b99b36555222e0a44ed1ec36787be3a9dd2dc
yamadapc <tacla.yamada@gmail.com>
2021-11-23 16:15:42 +1100

    Extract output file processor

--- crates/augmented/audio/audio-processor-file/src/lib.rs
+++ crates/augmented/audio/audio-processor-file/src/lib.rs
@@ -1,5 +1,7 @@
-mod audio_file_processor;
-
 pub use audio_file_processor::{
     file_io, AudioFileProcessor, AudioFileProcessorHandle, AudioFileSettings,
 };
+pub use output_file_processor::{OutputAudioFileProcessor, OutputFileSettings};
+
+mod audio_file_processor;
+mod output_file_processor;
