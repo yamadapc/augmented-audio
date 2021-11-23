4d80811d9aa5daaa3056ceec785b7c1d95296f32
yamadapc <tacla.yamada@gmail.com>
2021-11-24 10:23:13 +1100

    Try to fix unit-tests

--- crates/apps/daw/lib/state/wire/wire_native.dart
+++ crates/apps/daw/lib/state/wire/wire_native.dart
@@ -1,5 +1,7 @@
+import 'dart:developer';
 import 'dart:ffi';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter_daw_mock_ui/bridge_generated.dart';
 import 'package:flutter_daw_mock_ui/state/wire/wire_base.dart';
 
@@ -10,10 +12,19 @@ DawUi? initialize() {
     return dawUi!;
   }
 
-  DynamicLibrary dylib = DynamicLibrary.open(
-      "/Users/yamadapc/projects/rust-audio-software/target/debug/libdaw_ui.dylib");
-  dawUi = DawUi(dylib);
-  return dawUi!;
+  try {
+    DynamicLibrary dylib = DynamicLibrary.open(
+        "/Users/yamadapc/projects/rust-audio-software/target/debug/libdaw_ui.dylib");
+    dawUi = DawUi(dylib);
+    return dawUi!;
+  } catch (err) {
+    if (kDebugMode) {
+      log("Failed to initialize native library.");
+      return null;
+    } else {
+      rethrow;
+    }
+  }
 }
 
 class NativeAudioIOStore with AudioIOStore {
