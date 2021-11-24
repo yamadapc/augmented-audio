f90d9f1d28db7c24347b35d88cd613683a9dc2b7
yamadapc <tacla.yamada@gmail.com>
2021-11-24 13:22:14 +1100

    Minor refactoring / clean-up

--- crates/apps/daw/lib/ui/main_content_layout/midi_editor/ui/background/midi_note_lane_view.dart
+++ crates/apps/daw/lib/ui/main_content_layout/midi_editor/ui/background/midi_note_lane_view.dart
@@ -28,33 +28,44 @@ class MIDINoteLane extends StatelessWidget {
       ),
       width: double.infinity,
       child: Row(children: [
-        GestureDetector(
-          onPanUpdate: onSidebarPanUpdate,
-          child: Row(children: [
-            SizedBox(
-                width: 50,
-                child: Text(note.getSymbol(),
-                    style: DefaultTextStyle.of(context)
-                        .style
-                        .merge(TextStyle(fontSize: height / 1.5)))),
-            PianoKeyView(isSharp: note.isSharp(), height: height),
-          ]),
-        ),
-        Expanded(
-          child: GestureDetector(
-            onTapUp: (details) => onTapUp(context, details),
-            child: Container(
-              width: double.infinity,
-              height: height,
-              decoration: const BoxDecoration(
-                // Without painting, there's no gesture detection above
-                color: Colors.transparent,
-              ),
-              child: null,
-            ),
-          ),
-        )
+        buildSidebarRegion(context),
+        buildEmptyContentRegion(context)
       ]),
+    );
+  }
+
+  MouseRegion buildSidebarRegion(BuildContext context) {
+    return MouseRegion(
+      cursor: SystemMouseCursors.resizeUpDown,
+      child: GestureDetector(
+        onPanUpdate: onSidebarPanUpdate,
+        child: Row(children: [
+          SizedBox(
+              width: 50,
+              child: Text(note.getSymbol(),
+                  style: DefaultTextStyle.of(context)
+                      .style
+                      .merge(TextStyle(fontSize: height / 1.5)))),
+          PianoKeyView(isSharp: note.isSharp(), height: height),
+        ]),
+      ),
+    );
+  }
+
+  Expanded buildEmptyContentRegion(BuildContext context) {
+    return Expanded(
+      child: GestureDetector(
+        onTapUp: (details) => onTapUp(context, details),
+        child: Container(
+          width: double.infinity,
+          height: height,
+          decoration: const BoxDecoration(
+            // Without painting, there's no gesture detection above
+            color: Colors.transparent,
+          ),
+          child: null,
+        ),
+      ),
     );
   }
 
