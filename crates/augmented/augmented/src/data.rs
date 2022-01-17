5a0d4b9efec38b9b38f33f2f24216af019b4b0a5
yamadapc <tacla.yamada@gmail.com>
2022-01-17 08:03:29 -0300

    Prepare for publishing extra crates

--- crates/augmented/augmented/src/data.rs
+++ crates/augmented/augmented/src/data.rs
@@ -1,4 +1,5 @@
-pub use atomic_queue;
-pub use audio_volume as volume;
-pub use circular_data_structures;
 pub use smooth_value;
+
+pub use atomic_queue;
+pub use augmented_audio_volume as volume;
+pub use circular_data_structures;
