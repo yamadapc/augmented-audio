78d9843187cbca45dbae9c228ea6036d24326f79
yamadapc <tacla.yamada@gmail.com>
2022-02-03 18:00:01 +1100

    Remove unused bits from metronome app

--- crates/apps/metronome/macos/Runner/AppDelegate.swift
+++ crates/apps/metronome/macos/Runner/AppDelegate.swift
@@ -1,12 +1,10 @@
 import Cocoa
-import Firebase
 import FlutterMacOS
 
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
   override func applicationDidFinishLaunching(_ notification: Notification) {
     let result = dummy_method_to_enforce_bundling()
-    FirebaseApp.configure()
     print("Initialized \(result)")
   }
 
