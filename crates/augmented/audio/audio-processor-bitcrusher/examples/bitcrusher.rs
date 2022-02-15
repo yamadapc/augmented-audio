a24599f0667efa6cd0d25c88cd2833752b3373b0
yamadapc <tacla.yamada@gmail.com>
2022-02-15 17:14:38 +1100

    Update examples to run with/without GUI

--- crates/augmented/audio/audio-processor-bitcrusher/examples/bitcrusher.rs
+++ crates/augmented/audio/audio-processor-bitcrusher/examples/bitcrusher.rs
@@ -1,12 +1,19 @@
 use audio_processor_bitcrusher::BitCrusherProcessor;
 use audio_processor_traits::parameters::AudioProcessorHandleRef;
+use std::env::VarError;
 
 fn main() {
     let processor = BitCrusherProcessor::default();
     processor.handle().set_bit_rate(44100.0 / 4.0);
 
-    let handle: AudioProcessorHandleRef = Box::new(processor.generic_handle());
-    let _audio_handles = audio_processor_standalone::audio_processor_start(processor);
-
-    audio_processor_standalone::gui::open(handle);
+    match std::env::var("GUI") {
+        Ok(value) if value == "true" => {
+            let handle: AudioProcessorHandleRef = Box::new(processor.generic_handle());
+            let _audio_handles = audio_processor_standalone::audio_processor_start(processor);
+            audio_processor_standalone::gui::open(handle);
+        }
+        _ => {
+            audio_processor_standalone::audio_processor_main(processor);
+        }
+    }
 }
