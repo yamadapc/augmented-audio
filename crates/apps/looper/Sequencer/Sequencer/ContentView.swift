5e16d24d014c806ad273322a577189809e6f8620
yamadapc <tacla.yamada@gmail.com>
2022-02-28 18:11:07 +1100

    Initial Commit

---
+++ crates/apps/looper/Sequencer/Sequencer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sequencer
+//
+//  Created by Pedro Tacla Yamada on 28/2/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ crates/apps/looper/Sequencer/Sequencer/SequencerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SequencerApp.swift
+//  Sequencer
+//
+//  Created by Pedro Tacla Yamada on 28/2/2022.
+//
+
+import SwiftUI
+
+@main
+struct SequencerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
