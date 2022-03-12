969f39269bf1b20caeb9e948302199c5a1f7aee4
yamadapc <tacla.yamada@gmail.com>
2022-03-12 16:14:30 +1100

    Set-up auto-formatting

--- crates/apps/looper/Sequencer/SequencerTests/SequencerTests.swift
+++ crates/apps/looper/Sequencer/SequencerTests/SequencerTests.swift
@@ -5,11 +5,10 @@
 //  Created by Pedro Tacla Yamada on 28/2/2022.
 //
 
-import XCTest
 @testable import Sequencer
+import XCTest
 
 class SequencerTests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -28,9 +27,8 @@ class SequencerTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
--- crates/apps/looper/Sequencer/SequencerUITests/SequencerUITestsLaunchTests.swift
+++ crates/apps/looper/Sequencer/SequencerUITests/SequencerUITestsLaunchTests.swift
@@ -8,7 +8,6 @@
 import XCTest
 
 class SequencerUITestsLaunchTests: XCTestCase {
-
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
         true
     }
