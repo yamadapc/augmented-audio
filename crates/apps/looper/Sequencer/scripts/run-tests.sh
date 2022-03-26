ddbd096f81527c68fc9f80e692923336e05c8ba9
yamadapc <tacla.yamada@gmail.com>
2022-03-27 09:37:11 +1100

    Update test runner script

--- crates/apps/looper/Sequencer/scripts/run-tests.sh
+++ crates/apps/looper/Sequencer/scripts/run-tests.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
 rm -rf test-output
-make test
+
+echo "Running SequencerUI tests"
+xcodebuild -project Sequencer.xcodeproj -scheme "SequencerUI" test -resultBundlePath ./test-output/SequencerUI | xcbeautify
+echo "Running SequencerEngine tests"
+xcodebuild -project Sequencer.xcodeproj -scheme "SequencerEngine" test -resultBundlePath ./test-output/SequencerEngine | xcbeautify
+echo "Running Sequencer Mac tests"
+xcodebuild -project Sequencer.xcodeproj -scheme "Sequencer Mac" test -resultBundlePath ./test-output/SequencerMac | xcbeautify
+# echo "Running Sequencer tests"
+#xcodebuild -project Sequencer.xcodeproj -scheme "Sequencer" test -resultBundlePath ./test-output/Sequencer | xcbeautify
+
 xcparse screenshots --os --model --test-plan-config ./test-output/SequencerMac.xcresult ./test-output/SequencerMacScreenshots
 
 screenshot_path=$(find ./test-output/SequencerMacScreenshots | grep "Launch Screen")
