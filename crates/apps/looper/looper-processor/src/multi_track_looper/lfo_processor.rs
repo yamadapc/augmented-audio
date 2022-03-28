25fc5a3f91b13303659361429a1c628a5d55a69b
yamadapc <tacla.yamada@gmail.com>
2022-03-29 08:10:20 +1100

    Add unit-tests for LFO processor

--- crates/apps/looper/looper-processor/src/multi_track_looper/lfo_processor.rs
+++ crates/apps/looper/looper-processor/src/multi_track_looper/lfo_processor.rs
@@ -1,5 +1,6 @@
-use augmented_atomics::AtomicF32;
 use std::collections::HashMap;
+
+use augmented_atomics::AtomicF32;
 
 use crate::parameters::{build_default_parameters, build_parameter_indexes, ParameterId};
 
@@ -36,17 +37,17 @@ impl LFOHandle {
         self.frequency.get()
     }
 
-    pub fn modulation_amount(&self, parameter_id: &ParameterId) -> f32 {
-        let index = self.indexes[parameter_id];
-        self.values[index].get()
-    }
-
     pub fn set_amount(&self, value: f32) {
         self.amount.set(value);
     }
 
     pub fn set_frequency(&self, value: f32) {
         self.frequency.set(value);
+    }
+
+    pub fn modulation_amount(&self, parameter_id: &ParameterId) -> f32 {
+        let index = self.indexes[parameter_id];
+        self.values[index].get()
     }
 
     pub fn set_parameter_map(&self, parameter_id: ParameterId, amount: Option<f32>) {
@@ -56,5 +57,56 @@ impl LFOHandle {
         } else {
             self.values[index].set(0.0);
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::parameters::SourceParameter;
+    use audio_processor_testing_helpers::assert_f_eq;
+
+    use super::*;
+
+    #[test]
+    fn test_create_lfo_handle() {
+        let _handle = LFOHandle::default();
+    }
+
+    #[test]
+    fn test_set_amount() {
+        let handle = LFOHandle::default();
+        handle.set_amount(0.88);
+        assert_f_eq!(handle.amount(), 0.88);
+    }
+
+    #[test]
+    fn test_set_frequency() {
+        let handle = LFOHandle::default();
+        handle.set_frequency(44.44);
+        assert_f_eq!(handle.frequency(), 44.44);
+    }
+
+    #[test]
+    fn test_add_modulation() {
+        let handle = LFOHandle::default();
+        let amount = handle.modulation_amount(&SourceParameter::Start.into());
+        assert_f_eq!(amount, 0.0);
+
+        handle.set_parameter_map(SourceParameter::Start.into(), Some(0.5));
+
+        let amount = handle.modulation_amount(&SourceParameter::Start.into());
+        assert_f_eq!(amount, 0.5);
+    }
+
+    #[test]
+    fn test_remove_modulation() {
+        let handle = LFOHandle::default();
+        let amount = handle.modulation_amount(&SourceParameter::Start.into());
+
+        handle.set_parameter_map(SourceParameter::Start.into(), Some(0.5));
+        handle.set_parameter_map(SourceParameter::Start.into(), None);
+
+        let amount = handle.modulation_amount(&SourceParameter::Start.into());
+        assert_f_eq!(amount, 0.0);
     }
 }
