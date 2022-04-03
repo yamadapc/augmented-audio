1d1ad5ad16ca90da1a622c7c7d9537f774303db1
yamadapc <tacla.yamada@gmail.com>
2022-04-03 19:12:16 +1000

    Updates for codegen poc

--- crates/apps/looper/augmented-codegen/src/generate_swift_opaque_value.rs
+++ crates/apps/looper/augmented-codegen/src/generate_swift_opaque_value.rs
@@ -1,16 +1,17 @@
+use std::fmt::format;
 use std::ops::Add;
 
 use super::CodegenOutput;
 
 pub struct OpaqueValueInput {
-    identifier: String,
+    pub identifier: String,
 }
 
 pub struct OpaqueValueMethod {
-    parent: String,
-    identifier: String,
-    arguments: Vec<String>,
-    return_value: String,
+    pub parent: String,
+    pub identifier: String,
+    pub arguments: Vec<(String, String)>,
+    pub return_value: Option<String>,
 }
 
 pub fn generate_opaque_value(input: OpaqueValueInput) -> CodegenOutput {
@@ -18,7 +19,7 @@ pub fn generate_opaque_value(input: OpaqueValueInput) -> CodegenOutput {
     rust_code += &*format!(
         r#"
 #[no_mangle]
-pub extern "C" fn {}__delete(ptr: *mut {}) {{
+pub extern "C" fn boxed__{}__delete(ptr: *mut {}) {{
     let _ = unsafe {{ Box::from_raw(ptr) }};
 }}
 "#,
@@ -26,7 +27,7 @@ pub extern "C" fn {}__delete(ptr: *mut {}) {{
     );
     let destructor_c_name = format!("{}__delete", input.identifier);
 
-    let ident = "Native$".to_string() + &*input.identifier;
+    let ident = "Boxed$".to_string() + &*input.identifier;
     let mut swift_code = "".to_string();
     swift_code += &*format!("class {} {{\n", ident);
     swift_code += "    let __innerPtr: OpaquePointer\n";
@@ -40,16 +41,57 @@ pub extern "C" fn {}__delete(ptr: *mut {}) {{
     }
 }
 
-fn generate_opaque_method(value: OpaqueValueMethod) -> CodegenOutput {
+pub fn generate_opaque_method(value: OpaqueValueMethod) -> CodegenOutput {
     let mut rust_code = "".to_string();
+    let arguments = value
+        .arguments
+        .iter()
+        .map(|(name, ty)| format!("{}: {}", name, ty))
+        .collect::<Vec<String>>();
+    let arguments = {
+        if !arguments.is_empty() {
+            let s = arguments.join(", ");
+            ", ".to_string() + &*s
+        } else {
+            "".to_string()
+        }
+    };
+    let argument_names = value
+        .arguments
+        .iter()
+        .map(|(name, _)| name.clone())
+        .collect::<Vec<String>>();
+    let argument_names = if argument_names.is_empty() {
+        "".to_string()
+    } else {
+        ", ".to_string() + &*argument_names.join(", ")
+    };
+    let return_value = value
+        .return_value
+        .as_ref()
+        .map(|v| format!(" -> *mut {}", v))
+        .unwrap_or("".to_string());
+
+    let body = if value.return_value.is_some() {
+        format!(
+            r#"let result = {}::{}(value{});
+    Box::into_raw(Box::new(result))"#,
+            value.parent, value.identifier, argument_names
+        )
+    } else {
+        format!(
+            "{}::{}(value{})",
+            value.parent, value.identifier, argument_names
+        )
+    };
     rust_code += &*format!(
         r#"
-pub extern "C" fn {}__{}(ptr: *mut {}) {{
+pub extern "C" fn boxed__{}__{}(ptr: *mut {}{}){} {{
     let value: &{} = unsafe {{ &(*ptr) }};
-    {}::{}(value);
+    {}
 }}
 "#,
-        value.parent, value.identifier, value.parent, value.parent, value.parent, value.identifier
+        value.parent, value.identifier, value.parent, arguments, return_value, value.parent, body
     );
     CodegenOutput {
         rust_code,
@@ -67,14 +109,56 @@ mod test {
             parent: "LooperEngine".to_string(),
             identifier: "trigger".to_string(),
             arguments: vec![],
-            return_value: "".to_string(),
+            return_value: None,
         });
         assert_eq!(
             opaque_value.rust_code,
             r#"
-pub extern "C" fn LooperEngine__trigger(ptr: *mut LooperEngine) {
+pub extern "C" fn boxed__LooperEngine__trigger(ptr: *mut LooperEngine) {
     let value: &LooperEngine = unsafe { &(*ptr) };
     LooperEngine::trigger(value);
+}
+"#
+        );
+    }
+
+    #[test]
+    fn test_generate_opaque_method_rust_with_arguments() {
+        let opaque_value = generate_opaque_method(OpaqueValueMethod {
+            parent: "LooperEngine".to_string(),
+            identifier: "trigger".to_string(),
+            arguments: vec![
+                ("something".to_string(), "f32".to_string()),
+                ("something_else".to_string(), "i32".to_string()),
+            ],
+            return_value: None,
+        });
+        assert_eq!(
+            opaque_value.rust_code,
+            r#"
+pub extern "C" fn boxed__LooperEngine__trigger(ptr: *mut LooperEngine, something: f32, something_else: i32) {
+    let value: &LooperEngine = unsafe { &(*ptr) };
+    LooperEngine::trigger(value, something, something_else);
+}
+"#
+        );
+    }
+
+    #[test]
+    fn test_generate_opaque_method_rust_with_return_value() {
+        let opaque_value = generate_opaque_method(OpaqueValueMethod {
+            parent: "LooperEngine".to_string(),
+            identifier: "trigger".to_string(),
+            arguments: vec![],
+            return_value: Some("SomeOtherValue".to_string()),
+        });
+        assert_eq!(
+            opaque_value.rust_code,
+            r#"
+pub extern "C" fn boxed__LooperEngine__trigger(ptr: *mut LooperEngine) -> *mut SomeOtherValue {
+    let value: &LooperEngine = unsafe { &(*ptr) };
+    let result = LooperEngine::trigger(value);
+    Box::into_raw(Box::new(result))
 }
 "#
         );
@@ -89,7 +173,7 @@ pub extern "C" fn LooperEngine__trigger(ptr: *mut LooperEngine) {
             opaque_value.rust_code,
             r#"
 #[no_mangle]
-pub extern "C" fn LooperEngine__delete(ptr: *mut LooperEngine) {
+pub extern "C" fn boxed__LooperEngine__delete(ptr: *mut LooperEngine) {
     let _ = unsafe { Box::from_raw(ptr) };
 }
 "#
@@ -103,7 +187,7 @@ pub extern "C" fn LooperEngine__delete(ptr: *mut LooperEngine) {
         });
         assert_eq!(
             opaque_value.swift_code,
-            r"class Native$LooperEngine {
+            r"class Boxed$LooperEngine {
     let __innerPtr: OpaquePointer
     init(rawPtr: OpaquePointer) { self.__innerPtr = rawPtr }
     deinit { LooperEngine__delete(self.__innerPtr) }
--- crates/apps/looper/augmented-codegen/src/main.rs
+++ crates/apps/looper/augmented-codegen/src/main.rs
@@ -1,7 +1,3 @@
-mod generate_swift_enum;
-mod generate_swift_opaque_value;
-
-use complexity::Complexity;
 use std::env;
 use std::fs::File;
 use std::io::Read;
@@ -9,11 +5,21 @@ use std::ops::Deref;
 use std::path::PathBuf;
 use std::process;
 use std::process::id;
+
+use complexity::Complexity;
 use syn::visit::Visit;
 use syn::{
-    FnArg, Ident, ImplItem, ImplItemMethod, ItemImpl, ItemMod, ItemStruct, ItemUse, PatType, Type,
-    UseName, UsePath, UseTree, VisPublic, Visibility,
+    FnArg, Ident, ImplItem, ImplItemMethod, Item, ItemImpl, ItemMod, ItemStruct, ItemUse, Pat,
+    PatType, ReturnType, Type, UseName, UsePath, UseTree, VisPublic, Visibility,
 };
+
+use crate::generate_swift_enum::generate_swift_enum;
+use crate::generate_swift_opaque_value::{
+    generate_opaque_method, generate_opaque_value, OpaqueValueInput, OpaqueValueMethod,
+};
+
+mod generate_swift_enum;
+mod generate_swift_opaque_value;
 
 struct MethodSummary {
     ident: Ident,
@@ -134,7 +140,7 @@ fn lint_file(file: &syn::File) {
                                 );
                                 let complexity = method.complexity();
                                 if complexity > 4 {
-                                    println!("name={} complexity={}", name, complexity);
+                                    log::warn!("name={} complexity={}", name, complexity);
                                 }
                             }
                             _ => {}
@@ -147,7 +153,7 @@ fn lint_file(file: &syn::File) {
                 let name = item_fn.sig.ident.to_string();
                 let complexity = item_fn.complexity();
                 if complexity > 4 {
-                    println!("name={} complexity={}", name, complexity);
+                    log::warn!("name={} complexity={}", name, complexity);
                 }
             }
             _ => {}
@@ -172,33 +178,150 @@ fn main() {
     for filename in glob::glob(&*format!("{}/src/**/*.rs", crate_dir)).unwrap() {
         let filename = filename.unwrap();
         let mut file = File::open(&filename).expect("Unable to open file");
-        log::info!("Processing {}", filename.to_str().unwrap());
+        log::debug!("Processing {}", filename.to_str().unwrap());
 
         let mut src = String::new();
         file.read_to_string(&mut src).expect("Unable to read file");
 
         let file = syn::parse_file(&src).expect("Unable to parse file");
         lint_file(&file);
-        let mut visitor = CodegenVisitor::default();
-        visitor.visit_file(&file);
 
-        for struct_name in visitor.summaries {
-            println!("class Native${} {{", struct_name.ident);
-            println!("  var __nativePtr: OpaquePointer");
-            println!("  init() {{");
-            println!("      self.__nativePtr = __nativeInit__x1234()");
-            println!("  }}");
-            println!();
-            for method in struct_name.methods {
-                println!("  func {}() {{", method.ident);
-                println!("  }}");
+        let mut c_api_module = "".to_string();
+        let mut swift_module = "".to_string();
+        for item in file.items.iter() {
+            match item {
+                Item::Enum(en) => {
+                    log::info!("Running over {:?} / {}", filename, en.ident);
+                    let result = generate_swift_enum(en);
+                    combine_result(&mut c_api_module, &mut swift_module, result)
+                }
+                Item::Fn(_) => {}
+                Item::Impl(i) => {
+                    for item in &i.items {
+                        match item {
+                            ImplItem::Method(method) => {
+                                if let Some(result) = process_impl_method(i, method) {
+                                    combine_result(&mut c_api_module, &mut swift_module, result)
+                                }
+                            }
+                            _ => {}
+                        }
+                    }
+                }
+                Item::Struct(str) => {
+                    let result = generate_opaque_value(OpaqueValueInput {
+                        identifier: str.ident.to_string(),
+                    });
+                    combine_result(&mut c_api_module, &mut swift_module, result)
+                }
+                Item::Type(_) => {}
+                _ => {}
             }
-            println!();
-            println!("}}");
         }
+
+        let prefix = if filename.file_name().unwrap() == "mod.rs" {
+            filename.with_file_name("")
+        } else {
+            filename.with_extension("")
+        };
+        let c_api_filename = prefix.join("generated/c_api.rs");
+        if !c_api_module.is_empty() {
+            println!("// {}", c_api_filename.to_str().unwrap());
+            println!("{}", c_api_module);
+        }
+        let swift_filename = prefix.join(format!(
+            "generated/{}.swift",
+            prefix.file_name().unwrap().to_str().unwrap()
+        ));
+        if !swift_module.is_empty() {
+            println!("// {}", swift_filename.to_str().unwrap());
+            println!("{}", swift_module);
+        }
+
+        // let mut visitor = CodegenVisitor::default();
+        // visitor.visit_file(&file);
+        // for struct_name in visitor.summaries {
+        //     println!("class Native${} {{", struct_name.ident);
+        //     println!("  var __nativePtr: OpaquePointer");
+        //     println!("  init() {{");
+        //     println!("      self.__nativePtr = __nativeInit__x1234()");
+        //     println!("  }}");
+        //     println!();
+        //     for method in struct_name.methods {
+        //         println!("  func {}() {{", method.ident);
+        //         println!("  }}");
+        //     }
+        //     println!();
+        //     println!("}}");
+        // }
     }
 }
 
+fn process_impl_method(i: &ItemImpl, method: &ImplItemMethod) -> Option<CodegenOutput> {
+    let parent = match i.self_ty.deref() {
+        Type::Path(path) => path.path.get_ident()?.to_string(),
+        _ => return None,
+    };
+
+    if method.sig.inputs.len() == 0 {
+        // Check if this is a constructor we can support
+        return None;
+    }
+
+    let first_input = &method.sig.inputs[0];
+    if matches!(first_input, FnArg::Receiver(_)) {
+        let arguments = method
+            .sig
+            .inputs
+            .iter()
+            .skip(1)
+            .filter_map(|input| {
+                if let FnArg::Typed(PatType { pat, ty, .. }) = input {
+                    match (pat.deref(), ty.deref()) {
+                        (Pat::Ident(ident), Type::Path(pth)) => {
+                            Some((ident.ident.to_string(), pth.path.get_ident()?.to_string()))
+                        }
+                        // todo bail out of the whole function
+                        _ => None,
+                    }
+                } else {
+                    None
+                }
+            })
+            .collect();
+        let return_value = match &method.sig.output {
+            ReturnType::Default => None,
+            ReturnType::Type(_, ty) => match ty.deref() {
+                Type::Path(pth) => Some(pth.path.get_ident()?.to_string()),
+                // todo support other patterns
+                _ => return None,
+            },
+        };
+        let result = generate_opaque_method(OpaqueValueMethod {
+            parent,
+            identifier: method.sig.ident.to_string(),
+            arguments,
+            return_value,
+        });
+        Some(result)
+    } else {
+        // Check if this is a constructor
+        None
+    }
+}
+
+fn combine_result(c_api_module: &mut String, swift_module: &mut String, result: CodegenOutput) {
+    if !result.rust_code.is_empty() {
+        *c_api_module += "\n";
+        *c_api_module += &*result.rust_code;
+    }
+    if !result.swift_code.is_empty() {
+        *swift_module += "\n";
+        *swift_module += &*result.swift_code;
+    }
+}
+
+#[derive(Default)]
 pub struct CodegenOutput {
     rust_code: String,
     swift_code: String,
