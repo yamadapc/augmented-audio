ded2b953ff4a2997ef879af912a8a5ad4a94ff25
yamadapc <tacla.yamada@gmail.com>
2022-04-04 17:34:30 +1000

    Update time-info-provider with more unit-tests

--- crates/apps/looper/looper-processor/src/audio/time_info_provider/mod.rs
+++ crates/apps/looper/looper-processor/src/audio/time_info_provider/mod.rs
@@ -1,12 +1,14 @@
+//! Looper representation of the play-head state. An abstract [`TimeInfo`] provider
+//! [`TimeInfoProvider`] wraps both `vst` (slave mode) and [`PlayHead`] (master mode).
 use std::sync::atomic::{AtomicBool, Ordering};
 
 use derive_builder::Builder;
 use mockall::automock;
 
 #[cfg(not(target_os = "ios"))]
-pub use audio_processor_standalone::standalone_vst::vst::plugin::HostCallback;
+use audio_processor_standalone::standalone_vst::vst;
 #[cfg(not(target_os = "ios"))]
-use audio_processor_standalone::{standalone_vst::vst, standalone_vst::vst::host::Host};
+pub use audio_processor_standalone::standalone_vst::vst::plugin::HostCallback;
 use augmented_atomics::AtomicValue;
 use augmented_playhead::{PlayHead, PlayHeadOptions};
 use metronome::MetronomePlayhead;
@@ -14,6 +16,8 @@ use metronome::MetronomePlayhead;
 #[cfg(target_os = "ios")]
 pub type HostCallback = ();
 
+/// Represents instantaneous play-head state. The current tempo, position and "is playing" state can
+/// be queried from this object.
 #[derive(Builder)]
 pub struct TimeInfo {
     tempo: Option<f64>,
@@ -23,33 +27,55 @@ pub struct TimeInfo {
 }
 
 impl TimeInfo {
+    /// Current tempo if set
     pub fn tempo(&self) -> Option<f64> {
         self.tempo
     }
 
+    /// Current position as a fraction of samples
     pub fn position_samples(&self) -> f64 {
         self.position_samples
     }
 
+    /// Current position in nº of beats
     pub fn position_beats(&self) -> Option<f64> {
         self.position_beats
     }
 
+    /// Whether the playhead is moving
     pub fn is_playing(&self) -> bool {
         self.is_playing
     }
 }
 
+/// Abstract `TimeInfo` provider trait. This is useful for mocking out the playhead on unit-tests.
 #[automock]
 pub trait TimeInfoProvider {
+    /// The current [`TimeInfo`] object
     fn get_time_info(&self) -> TimeInfo;
-    fn tick(&self);
-    // Transport control; this should only be enabled when the looper is the master tempo
+    /// Must be called for each sample, this moves the playhead
+    fn tick(&self) {
+        self.tick_n(1)
+    }
+    /// Alternative for [`TimeInfoProvider::tick`], should move the playhead by `num_samples`
+    fn tick_n(&self, num_samples: u32);
+    /// Transport control, should start the playhead
     fn play(&self);
+    /// Transport control, should stop the playhead (pause & reset to 0)
     fn stop(&self);
+    /// Transport control, should pause the playhead
     fn pause(&self);
 }
 
+/// Concrete [`TimeInfoProvider`] implementation that supports both a "hosted" mode for VST where
+/// the playhead state is queried from the VST Host and a "stand-alone" mode, where the playhead
+/// state is managed internally by using [`PlayHead`].
+///
+/// On `ios` only stand-alone mode is supported as the `vst` crate can't work.
+///
+/// Transport control methods only work if we're in stand-alone mode. It potentially would be
+/// possible to change the host's playhead via the VST APIs, but that has not been implemented or
+/// investigated.
 pub struct TimeInfoProviderImpl {
     host_callback: Option<HostCallback>,
     playhead: PlayHead,
@@ -64,29 +90,14 @@ impl TimeInfoProvider for TimeInfoProviderImpl {
 
     #[cfg(not(target_os = "ios"))]
     fn get_time_info(&self) -> TimeInfo {
-        let host_time_info = self
-            .host_callback
-            .and_then(|cb| {
-                cb.get_time_info(
-                    (vst::api::TimeInfoFlags::TEMPO_VALID | vst::api::TimeInfoFlags::PPQ_POS_VALID)
-                        .bits(),
-                )
-            })
-            .map(|vst_time_info| TimeInfo {
-                tempo: Some(vst_time_info.tempo),
-                position_samples: vst_time_info.sample_pos,
-                position_beats: Some(vst_time_info.ppq_pos),
-                is_playing: (vst_time_info.flags
-                    & vst::api::TimeInfoFlags::TRANSPORT_PLAYING.bits())
-                    != 0,
-            });
+        let host_time_info = get_host_time_info(self.host_callback.as_ref());
 
         host_time_info.unwrap_or_else(|| self.playhead_timeinfo())
     }
 
-    fn tick(&self) {
+    fn tick_n(&self, n: u32) {
         if self.is_playing.load(Ordering::Relaxed) {
-            self.playhead.accept_samples(1);
+            self.playhead.accept_samples(n);
         }
     }
 
@@ -105,6 +116,7 @@ impl TimeInfoProvider for TimeInfoProviderImpl {
 }
 
 impl TimeInfoProviderImpl {
+    /// Create provider with given vst [`HostCallback`] or no callback.
     pub fn new(host_callback: Option<HostCallback>) -> Self {
         TimeInfoProviderImpl {
             host_callback,
@@ -139,6 +151,28 @@ impl TimeInfoProviderImpl {
     }
 }
 
+/// Return the VST API time-info, converted into our object.
+#[cfg(not(target_os = "ios"))]
+fn get_host_time_info<H: vst::host::Host>(host: Option<&H>) -> Option<TimeInfo> {
+    host.as_ref()
+        .and_then(|cb| {
+            cb.get_time_info(
+                (vst::api::TimeInfoFlags::TEMPO_VALID | vst::api::TimeInfoFlags::PPQ_POS_VALID)
+                    .bits(),
+            )
+        })
+        .map(|vst_time_info| TimeInfo {
+            tempo: Some(vst_time_info.tempo),
+            position_samples: vst_time_info.sample_pos,
+            position_beats: Some(vst_time_info.ppq_pos),
+            is_playing: (vst_time_info.flags & vst::api::TimeInfoFlags::TRANSPORT_PLAYING.bits())
+                != 0,
+        })
+}
+
+/// New-type for metronome compatibility, since metronome defines its own playhead logic.
+///
+/// This type implements [`MetronomePlayhead`] for a [`Shared`] reference of [`TimeInfoProvidedImpl`]
 pub struct TimeInfoMetronomePlayhead(pub audio_garbage_collector::Shared<TimeInfoProviderImpl>);
 
 impl MetronomePlayhead for TimeInfoMetronomePlayhead {
@@ -149,6 +183,7 @@ impl MetronomePlayhead for TimeInfoMetronomePlayhead {
 
 #[cfg(test)]
 mod test {
+    use audio_garbage_collector::make_shared;
     use audio_processor_testing_helpers::assert_f_eq;
 
     use super::*;
@@ -170,6 +205,55 @@ mod test {
     }
 
     #[test]
+    fn test_time_info_provider_stop_will_reset_playhead() {
+        let time_info_provider = TimeInfoProviderImpl::new(None);
+        time_info_provider.play();
+        time_info_provider.set_sample_rate(1000.0);
+        time_info_provider.set_tempo(60.0);
+        // jump 4 seconds in
+        time_info_provider.tick_n(4000);
+        let position_beats = time_info_provider.get_time_info().position_beats.unwrap();
+        assert_f_eq!(position_beats, 4.0);
+        time_info_provider.stop();
+        let position_beats = time_info_provider.get_time_info().position_beats.unwrap();
+        assert_f_eq!(position_beats, 0.0);
+    }
+
+    #[test]
+    fn test_time_info_provider_stop_will_prevent_the_playhead_from_updating() {
+        let time_info_provider = TimeInfoProviderImpl::new(None);
+        time_info_provider.play();
+        time_info_provider.set_sample_rate(1000.0);
+        time_info_provider.set_tempo(60.0);
+        // jump 4 seconds in
+        time_info_provider.tick_n(4000);
+        let position_beats = time_info_provider.get_time_info().position_beats.unwrap();
+        assert_f_eq!(position_beats, 4.0);
+        time_info_provider.stop();
+        // jump 4 more seconds in
+        time_info_provider.tick_n(4000);
+        let position_beats = time_info_provider.get_time_info().position_beats.unwrap();
+        assert_f_eq!(position_beats, 0.0);
+    }
+
+    #[test]
+    fn test_time_info_provider_pause_will_prevent_the_playhead_from_updating() {
+        let time_info_provider = TimeInfoProviderImpl::new(None);
+        time_info_provider.play();
+        time_info_provider.set_sample_rate(1000.0);
+        time_info_provider.set_tempo(60.0);
+        // jump 4 seconds in
+        time_info_provider.tick_n(4000);
+        let position_beats = time_info_provider.get_time_info().position_beats.unwrap();
+        assert_f_eq!(position_beats, 4.0);
+        time_info_provider.pause();
+        // jump 4 more seconds in
+        time_info_provider.tick_n(4000);
+        let position_beats = time_info_provider.get_time_info().position_beats.unwrap();
+        assert_f_eq!(position_beats, 4.0);
+    }
+
+    #[test]
     fn test_time_info_provider_with_tempo_keep_track_of_beats() {
         let time_info_provider = TimeInfoProviderImpl::new(None);
         time_info_provider.play();
@@ -188,5 +272,66 @@ mod test {
         assert!(time_info.position_beats().is_some());
         assert_f_eq!(time_info.position_beats().unwrap(), 1.0);
         assert_f_eq!(time_info.position_samples(), 100.0);
+    }
+
+    #[test]
+    fn test_metronome_playhead() {
+        let time_info_provider = TimeInfoProviderImpl::new(None);
+        let time_info_provider = make_shared(time_info_provider);
+        let metronome_playhead = TimeInfoMetronomePlayhead(time_info_provider.clone());
+        assert_f_eq!(metronome_playhead.position_beats(), 0.0);
+        time_info_provider.play();
+        time_info_provider.set_sample_rate(1000.0);
+        time_info_provider.set_tempo(60.0);
+        time_info_provider.tick_n(4000);
+        assert_f_eq!(metronome_playhead.position_beats(), 4.0);
+    }
+
+    #[test]
+    fn test_get_host_time_info_when_theres_no_host_returns_none() {
+        let result = get_host_time_info::<vst::plugin::HostCallback>(None);
+        assert!(result.is_none());
+    }
+
+    #[test]
+    fn test_get_host_time_info_when_host_returns_none_returns_none() {
+        struct MockHost {}
+
+        impl vst::host::Host for MockHost {
+            fn get_time_info(&self, _mask: i32) -> Option<vst::api::TimeInfo> {
+                None
+            }
+        }
+
+        let host = MockHost {};
+        let result = get_host_time_info(Some(&host));
+        assert!(result.is_none());
+    }
+
+    #[test]
+    fn test_get_host_time_info_converts_vst_time_info() {
+        struct MockHost {}
+
+        impl vst::host::Host for MockHost {
+            fn get_time_info(&self, _mask: i32) -> Option<vst::api::TimeInfo> {
+                Some(vst::api::TimeInfo {
+                    ppq_pos: 8.0,
+                    tempo: 120.0,
+                    sample_pos: 1000.0,
+                    flags: vst::api::TimeInfoFlags::TRANSPORT_PLAYING.bits()
+                        | vst::api::TimeInfoFlags::TRANSPORT_RECORDING.bits(),
+                    ..Default::default()
+                })
+            }
+        }
+
+        let host = MockHost {};
+        let result = get_host_time_info(Some(&host));
+        assert!(result.is_some());
+        let result = result.unwrap();
+        assert_f_eq!(result.tempo().unwrap(), 120.0);
+        assert_f_eq!(result.position_beats().unwrap(), 8.0);
+        assert_f_eq!(result.position_samples(), 1000.0);
+        assert_eq!(result.is_playing(), true);
     }
 }
