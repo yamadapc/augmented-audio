6c8a7204f6bdcaff68511ae08ccb645d171b8b79
yamadapc <tacla.yamada@gmail.com>
2022-04-06 21:16:55 +1000

    Update store comments

--- crates/apps/looper/Sequencer/SequencerUI/Sources/SequencerUI/store/Store.swift
+++ crates/apps/looper/Sequencer/SequencerUI/Sources/SequencerUI/store/Store.swift
@@ -15,12 +15,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // = /copyright ===================================================================
-//
-//  File.swift
-//
-//
-//  Created by Pedro Tacla Yamada on 11/3/2022.
-//
 
 import Combine
 import CoreGraphics
