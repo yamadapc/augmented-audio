b02445f2a58b9723b92c11e7668091bb8b71d203
yamadapc <tacla.yamada@gmail.com>
2022-04-07 20:52:32 +1000

    Update lib.rs

--- crates/augmented/audio/adsr-envelope/src/lib.rs
+++ crates/augmented/audio/adsr-envelope/src/lib.rs
@@ -25,7 +25,7 @@
 //! - - -
 //! Implementation of an ADSR envelope.
 //!
-//! ## Usage
+//! ## Basic usage
 //!
 //! ```rust
 //! use std::time::Duration;
@@ -33,19 +33,46 @@
 //! use augmented_adsr_envelope::Envelope;
 //!
 //! fn main() {
-//!   let mut envelope = Envelope::exp();
+//!   // Create an exponential envelope.
+//!   // The envelope configuration uses atomics, so it doesn't need
+//!   // to be an immutable reference.
+//!   let envelope = Envelope::exp();
 //!
+//!   // Set settings
 //!   envelope.set_sample_rate(1000.0);
 //!   envelope.set_attack(Duration::from_millis(200));
 //!
+//!   // Trigger the envelope
 //!   envelope.note_on();
 //!   for i in 0..10000 {
-//!     let volume = envelope.volume();
-//!     println!("{}", volume);
+//!     // Tick the envelope by 1 sample
 //!     envelope.tick();
+//!     // Get the current volume
+//!     let _volume = envelope.volume();
 //!   }
+//!   // Trigger the release stage
+//!   envelope.note_off();
 //! }
 //! ```
+//!
+//! # Plots
+//! `Envelope::default();`
+//! ------------------
+//! | Attack  | 0.3  |
+//! | Decay   | 0.3  |
+//! | Sustain | 0.8  |
+//! | Release | 0.3  |
+//! ------------------
+//! ![](https://raw.githubusercontent.com/yamadapc/augmented-audio/master/crates/augmented/audio/adsr-envelope/src/__plots__/default-envelope.png)
+//!
+//! `Envelope::exp();`
+//! ------------------
+//! | Attack  | 0.3  |
+//! | Decay   | 0.3  |
+//! | Sustain | 0.8  |
+//! | Release | 0.3  |
+//! ------------------
+//! ![](https://raw.githubusercontent.com/yamadapc/augmented-audio/master/crates/augmented/audio/adsr-envelope/src/__plots__/exp-envelope.png)
 
 use augmented_atomics::{AtomicEnum, AtomicF32};
 use num_derive::{FromPrimitive, ToPrimitive};
