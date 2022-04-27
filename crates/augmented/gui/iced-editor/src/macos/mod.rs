8ccc21afef0ccc19749b19ea0ef8309728b1bd4e
yamadapc <tacla.yamada@gmail.com>
2022-04-28 08:39:11 +1000

    Bump iced-editor dependencies

--- crates/augmented/gui/iced-editor/src/macos/mod.rs
+++ crates/augmented/gui/iced-editor/src/macos/mod.rs
@@ -21,16 +21,15 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 use cocoa::base::id;
-use raw_window_handle::macos::MacOSHandle;
+use raw_window_handle::AppKitHandle;
 use raw_window_handle::RawWindowHandle;
 use std::ffi::c_void;
 
 pub fn get_raw_window_handle(parent: *mut c_void) -> RawWindowHandle {
     let parent_id = parent as id;
     let parent_window = unsafe { msg_send![parent_id, window] };
-    RawWindowHandle::MacOS(MacOSHandle {
-        ns_window: parent_window,
-        ns_view: parent,
-        ..MacOSHandle::empty()
-    })
+    let mut handle = AppKitHandle::empty();
+    handle.ns_window = parent_window;
+    handle.ns_view = parent;
+    RawWindowHandle::AppKit(handle)
 }
