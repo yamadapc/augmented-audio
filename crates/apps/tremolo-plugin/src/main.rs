7ed8c3e262cb8ad9a9109d2f72bb2dbb7b9d79f7
yamadapc <tacla.yamada@gmail.com>
2022-05-21 07:14:26 +1000

    Try to make sure tremolo still compiles on linux

--- crates/apps/tremolo-plugin/src/main.rs
+++ crates/apps/tremolo-plugin/src/main.rs
@@ -1,57 +1,62 @@
-use std::cell::RefCell;
-use std::ffi::c_void;
-use std::ops::Deref;
 use std::sync::Arc;
-
-use cacao::layout::Layout;
-use cacao::macos::window::Window;
-use cacao::macos::{App, AppDelegate};
-use cacao::view::View;
-use objc::{msg_send, sel, sel_impl};
-use vst::editor::Editor;
 
 use audio_processor_standalone::standalone_processor::StandaloneOptions;
 use audio_processor_standalone::StandaloneAudioOnlyProcessor;
-use generic_parameters_editor::GenericParametersEditor;
 use tas_v2::build_parameters_editor;
 use tas_v2::config::get_configuration_root_path;
 use tas_v2::config::logging::configure_logging;
 use tas_v2::parameters::build_parameters;
 use tas_v2::processor::Processor;
 
-struct TremoloApp {
-    editor: RefCell<GenericParametersEditor>,
-    content_view: View,
-    window: Window,
-}
+#[cfg(target_os = "macos")]
+mod app {
+    use std::cell::RefCell;
+    use std::ffi::c_void;
+    use std::ops::Deref;
 
-impl TremoloApp {
-    fn new(editor: GenericParametersEditor) -> Self {
-        Self {
-            editor: editor.into(),
-            content_view: View::new(),
-            window: Window::default(),
+    use cacao::layout::Layout;
+    use cacao::macos::window::Window;
+    use cacao::macos::AppDelegate;
+    use cacao::view::View;
+    use objc::{msg_send, sel, sel_impl};
+    use vst::editor::Editor;
+
+    use generic_parameters_editor::GenericParametersEditor;
+
+    pub struct TremoloApp {
+        editor: RefCell<GenericParametersEditor>,
+        content_view: View,
+        window: Window,
+    }
+
+    impl TremoloApp {
+        pub fn new(editor: GenericParametersEditor) -> Self {
+            Self {
+                editor: editor.into(),
+                content_view: View::new(),
+                window: Window::default(),
+            }
         }
     }
-}
 
-impl AppDelegate for TremoloApp {
-    fn did_finish_launching(&self) {
-        self.window.set_minimum_content_size(400., 400.);
-        self.window.set_title("Hello World!");
+    impl AppDelegate for TremoloApp {
+        fn did_finish_launching(&self) {
+            self.window.set_minimum_content_size(400., 400.);
+            self.window.set_title("Hello World!");
 
-        self.window.set_content_view(&self.content_view);
-        self.content_view
-            .set_translates_autoresizing_mask_into_constraints(true);
-        self.window.show();
-        self.window.make_key_and_order_front();
+            self.window.set_content_view(&self.content_view);
+            self.content_view
+                .set_translates_autoresizing_mask_into_constraints(true);
+            self.window.show();
+            self.window.make_key_and_order_front();
 
-        self.content_view.objc.get(|id| unsafe {
-            let id = id as *const _ as cocoa::base::id;
-            let window_id: cocoa::base::id = msg_send![id, window];
-            assert_eq!(self.window.objc.deref() as *const _, window_id);
-            self.editor.borrow_mut().open(id as *mut c_void);
-        });
+            self.content_view.objc.get(|id| unsafe {
+                let id = id as *const _ as cocoa::base::id;
+                let window_id: cocoa::base::id = msg_send![id, window];
+                assert_eq!(self.window.objc.deref() as *const _, window_id);
+                self.editor.borrow_mut().open(id as *mut c_void);
+            });
+        }
     }
 }
 
@@ -71,5 +76,9 @@ fn main() {
 
     let editor = build_parameters_editor(&parameters);
 
-    App::new("com.beijaflor.tasv2", TremoloApp::new(editor)).run();
+    #[cfg(target_os = "macos")]
+    cacao::macos::App::new("com.beijaflor.tasv2", app::TremoloApp::new(editor)).run();
+
+    #[cfg(not(target_os = "macos"))]
+    std::thread::park();
 }
