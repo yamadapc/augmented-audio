0421e037830a8b907fc8a538bdb090e82c5ab82e
yamadapc <tacla.yamada@gmail.com>
2022-05-25 08:46:41 +1000

    Improve docs

--- crates/augmented/audio/audio-garbage-collector/src/lib.rs
+++ crates/augmented/audio/audio-garbage-collector/src/lib.rs
@@ -20,6 +20,16 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
+
+//! Batteries included solution to using reference counted values on the audio-thread.
+//!
+//! Wraps `basedrop` so that smart pointers are dropped on a background thread. Exposes a default
+//! global GC thread and helpers to create pointers attached to it.
+//!
+//! # Collection frequency
+//! Collection is based on polling the queue. If references are created and dropped very frequently
+//! this will not be adequate.
+
 use std::sync::{Arc, Mutex};
 use std::thread::JoinHandle;
 use std::time::Duration;
@@ -43,11 +53,13 @@ pub fn handle() -> &'static Handle {
     GARBAGE_COLLECTOR.handle()
 }
 
+/// Create a new [`basedrop::SharedCell`] value using the default global [`GarbageCollector`]
+/// instance.
 pub fn make_shared_cell<T: Send + 'static>(value: T) -> SharedCell<T> {
     SharedCell::new(make_shared(value))
 }
 
-/// Create a new `basedrop::Shared` value using the default global `GarbageCollector`
+/// Create a new [`basedrop::Shared`] value using the default global [`GarbageCollector`]
 /// instance.
 pub fn make_shared<T: Send + 'static>(value: T) -> Shared<T> {
     Shared::new(handle(), value)
