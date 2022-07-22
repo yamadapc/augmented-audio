857e1b31fbdcf76ad9b0b51c50c7d1201320511d
yamadapc <tacla.yamada@gmail.com>
2022-07-22 12:40:58 +1000

    Add missing unit-tests

--- crates/apps/plugin-host/plugin-host-lib/src/commands/main/file_watch.rs
+++ crates/apps/plugin-host/plugin-host-lib/src/commands/main/file_watch.rs
@@ -23,24 +23,37 @@
 use std::path::Path;
 use std::sync::mpsc::Receiver;
 
-use actix::Addr;
+use actix::Recipient;
 use notify::DebouncedEvent;
 
-use crate::audio_io::test_plugin_host::TestPluginHost;
 use crate::audio_io::LoadPluginMessage;
-use crate::commands::options::RunOptions;
 
+#[mockall::automock]
+pub trait LoadPluginSink {
+    fn push(&self, msg: LoadPluginMessage);
+}
+
+impl LoadPluginSink for Recipient<LoadPluginMessage> {
+    fn push(&self, msg: LoadPluginMessage) {
+        self.do_send(msg);
+    }
+}
+
+// TODO:
+// * This should signal when ready
+// * This should accept a stop signal
+// * Should probably use an actor instead of looking as is
 pub fn run_file_watch_loop(
     rx: Receiver<DebouncedEvent>,
-    run_options: RunOptions,
-    host: Addr<TestPluginHost>,
+    plugin_path: &Path,
+    recipient: impl LoadPluginSink,
 ) -> ! {
     let inner = || -> Result<(), std::io::Error> {
-        let mut current_hash = get_file_hash(run_options.plugin_path().as_ref())?;
+        let mut current_hash = get_file_hash(plugin_path)?;
         loop {
             match rx.recv() {
                 Ok(_) => {
-                    let new_hash = get_file_hash(run_options.plugin_path().as_ref())?;
+                    let new_hash = get_file_hash(plugin_path)?;
                     if new_hash == current_hash {
                         log::warn!("Ignoring event due to same plugin hash");
                         continue;
@@ -52,9 +65,8 @@ pub fn run_file_watch_loop(
                         current_hash = new_hash;
                     }
 
-                    // TODO - How to get the result from an arbitrary thread?
-                    host.do_send(LoadPluginMessage {
-                        plugin_path: Path::new(run_options.plugin_path()).into(),
+                    recipient.push(LoadPluginMessage {
+                        plugin_path: Path::new(plugin_path).into(),
                     });
                 }
                 Err(err) => log::error!("File watch error: {}", err),
@@ -73,4 +85,59 @@ fn get_file_hash(path: &Path) -> Result<String, std::io::Error> {
     let file_contents = std::fs::read(path)?;
     let digest = md5::compute(file_contents);
     Ok(format!("{:x}", digest))
+}
+
+#[cfg(test)]
+mod test {
+    use std::sync::mpsc::channel;
+    use std::time::Duration;
+
+    use super::*;
+
+    #[test]
+    fn test_get_file_hash() {
+        let data_path =
+            tempdir::TempDir::new("plugin_host_lib__file_watch__test_get_file_watch").unwrap();
+        let test_file_path = data_path.path().join("test.txt");
+
+        let test_str = "something";
+        std::fs::write(&test_file_path, test_str).unwrap();
+
+        let result = get_file_hash(&test_file_path).unwrap();
+        assert_eq!(result, "437b930db84b8079c2dd804a71936b5f");
+    }
+
+    #[test]
+    fn test_run_file_watch_loop() {
+        let data_path =
+            tempdir::TempDir::new("plugin_host_lib__file_watch__test_get_file_watch").unwrap();
+        let test_file_path = data_path.path().join("test.txt");
+        std::fs::write(&test_file_path, "something").unwrap();
+
+        impl LoadPluginSink for std::sync::mpsc::Sender<LoadPluginMessage> {
+            fn push(&self, msg: LoadPluginMessage) {
+                self.send(msg).unwrap();
+            }
+        }
+
+        let (event_tx, event_rx) = channel();
+        let (sink_tx, sink_rx) = channel();
+
+        std::thread::spawn({
+            let test_file_path = test_file_path.clone();
+            move || run_file_watch_loop(event_rx, &test_file_path, sink_tx)
+        });
+
+        std::thread::sleep(Duration::from_millis(100));
+        std::fs::write(&test_file_path, "something-else").unwrap();
+        event_tx
+            .send(DebouncedEvent::Write(test_file_path.clone()))
+            .unwrap();
+        let _ = sink_rx.recv_timeout(Duration::from_millis(100)).unwrap();
+
+        event_tx
+            .send(DebouncedEvent::Write(test_file_path.clone()))
+            .unwrap();
+        assert!(sink_rx.recv_timeout(Duration::from_millis(100)).is_err());
+    }
 }
