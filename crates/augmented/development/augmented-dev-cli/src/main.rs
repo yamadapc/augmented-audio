07fa62a64fc0a678b6ac203ef6a0934f70c98fa7
yamadapc <tacla.yamada@gmail.com>
2022-09-16 19:33:31 +1000

    Bump vst and update documentation actions

--- crates/augmented/development/augmented-dev-cli/src/main.rs
+++ crates/augmented/development/augmented-dev-cli/src/main.rs
@@ -20,6 +20,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
+
 use crate::services::release_service::prerelease_all_crates;
 use crate::services::snapshot_tests_service::run_all_snapshot_tests;
 
