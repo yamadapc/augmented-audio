b050aa3a5fdd5b3681d07106f84920624423b19c
yamadapc <tacla.yamada@gmail.com>
2022-09-29 18:53:15 +1000

    Layout 1

---
+++ crates/augmented/gui/augmented-audio-wave/examples/rendering_tests.rs
@@ -0,0 +1,162 @@
+use cocoa::appkit::NSView;
+use cocoa::base::{id, YES};
+use core_graphics_types::geometry::CGSize;
+use foreign_types_shared::{ForeignType, ForeignTypeRef};
+use metal::{Device, MTLPixelFormat, MetalDrawableRef, MetalLayer};
+use objc::rc::autoreleasepool;
+use skia_safe::gpu::mtl::BackendContext;
+use skia_safe::gpu::{mtl, BackendRenderTarget, DirectContext, RecordingContext, SurfaceOrigin};
+use skia_safe::{
+    scalar, AlphaType, Budgeted, Canvas, Color4f, ColorType, ISize, ImageInfo, Paint, Path, Point,
+    SamplingOptions, Size, Surface,
+};
+use winit::dpi::PhysicalSize;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::ControlFlow;
+use winit::platform::macos::WindowExtMacOS;
+
+use audio_processor_file::AudioFileProcessor;
+use audio_processor_traits::{AudioBuffer, AudioProcessor, OwnedAudioBuffer, VecAudioBuffer};
+use augmented_audio_wave::widget::{App, DrawContext};
+
+fn main() {
+    wisual_logger::init_from_env();
+    let ev = winit::event_loop::EventLoop::new();
+    let window = winit::window::Window::new(&ev).unwrap();
+
+    let device = Device::system_default().unwrap();
+    let queue = device.new_command_queue();
+    let backend = unsafe {
+        BackendContext::new(
+            device.as_ptr() as mtl::Handle,
+            queue.as_ptr() as mtl::Handle,
+            std::ptr::null(),
+        )
+    };
+    let mut context = DirectContext::new_metal(&backend, None).unwrap();
+
+    let build_layer = || {
+        let draw_size = window.inner_size();
+        let layer = MetalLayer::new();
+        layer.set_device(&device);
+        layer.set_pixel_format(MTLPixelFormat::BGRA8Unorm);
+        layer.set_presents_with_transaction(false);
+
+        unsafe {
+            let view = window.ns_view() as id;
+            view.setWantsLayer(YES);
+            view.setLayer(layer.as_ref() as *const _ as _);
+        }
+
+        layer.set_drawable_size(CGSize::new(draw_size.width as f64, draw_size.height as f64));
+        layer
+    };
+    let metal_layer = build_layer();
+
+    let mut mouse_position = Point::new(0.0, 0.0);
+    let draw_size = window.inner_size();
+
+    let mut app = App::new();
+
+    log::info!("Starting to render");
+    ev.run(move |event, _target, control_flow| {
+        autoreleasepool(|| {
+            *control_flow = ControlFlow::Wait;
+
+            match event {
+                Event::WindowEvent { event, .. } => match event {
+                    WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                    WindowEvent::Touch(t) => {
+                        log::info!("{:?}", t);
+                    }
+                    WindowEvent::CursorMoved { position, .. } => {
+                        mouse_position.x = position.x as scalar;
+                        mouse_position.y = position.y as scalar;
+                        window.request_redraw();
+                    }
+                    WindowEvent::Resized(size) => {
+                        metal_layer
+                            .set_drawable_size(CGSize::new(size.width as f64, size.height as f64));
+                        window.request_redraw();
+                    }
+                    _ => (),
+                },
+                Event::RedrawRequested(_) => {
+                    get_drawable_surface(&metal_layer, &mut context).map(
+                        |(drawable, mut surface)| {
+                            let size = {
+                                let width = surface.width() as f32;
+                                let height = surface.height() as f32;
+                                augmented_audio_wave::widget::Size { width, height }
+                            };
+                            let canvas = surface.canvas();
+
+                            canvas.clear(Color4f::new(0.0, 0.0, 0.0, 1.0));
+                            let paint = Paint::new(Color4f::new(1.0, 0.0, 0.0, 1.0), None);
+
+                            app.draw(&mut DrawContext::new(canvas, size));
+                            canvas.draw_circle(mouse_position, 10.0, &paint);
+
+                            surface.flush_and_submit();
+                            drop(surface);
+
+                            let command_buffer = queue.new_command_buffer();
+                            command_buffer.present_drawable(drawable);
+                            command_buffer.commit();
+                        },
+                    );
+                }
+                _ => {}
+            }
+        });
+    });
+}
+
+fn get_drawable_surface<'a>(
+    metal_layer: &'a MetalLayer,
+    context: &'a mut DirectContext,
+) -> Option<(&'a MetalDrawableRef, Surface)> {
+    let drawable = metal_layer.next_drawable();
+    drawable.map(|drawable| (drawable, read_surface(context, &metal_layer, drawable)))
+}
+
+fn read_surface(
+    mut context: &mut DirectContext,
+    metal_layer: &MetalLayer,
+    drawable: &MetalDrawableRef,
+) -> Surface {
+    let drawable_size = {
+        let size = metal_layer.drawable_size();
+        Size::new(size.width as scalar, size.height as scalar)
+    };
+
+    unsafe {
+        let texture_info = mtl::TextureInfo::new(drawable.texture().as_ptr() as mtl::Handle);
+
+        let backend_render_target = BackendRenderTarget::new_metal(
+            (drawable_size.width as i32, drawable_size.height as i32),
+            1,
+            &texture_info,
+        );
+
+        Surface::from_backend_render_target(
+            &mut context,
+            &backend_render_target,
+            SurfaceOrigin::TopLeft,
+            ColorType::BGRA8888,
+            None,
+            None,
+        )
+        .unwrap()
+    }
+}
+
+#[allow(unused)]
+fn draw(canvas: &mut Canvas, path: &Path, mouse_position: Point) {
+    let mut paint = Paint::new(Color4f::new(1.0, 0.0, 0.0, 1.0), None);
+    paint.set_anti_alias(true);
+    canvas.draw_path(path, &paint);
+
+    let paint = Paint::new(Color4f::new(1.0, 0.0, 0.0, 1.0), None);
+    canvas.draw_circle(mouse_position, 30.0, &paint);
+}
--- crates/augmented/gui/augmented-audio-wave/src/widget.rs
+++ crates/augmented/gui/augmented-audio-wave/src/widget.rs
@@ -1,13 +1,169 @@
-use skia_safe::Canvas;
+use audio_processor_traits::Float;
+use skia_safe::{Canvas, Color4f, Paint};
+
+pub struct App {}
+
+impl App {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn draw(&self, draw_context: &mut DrawContext) {
+        let mut children = vec![
+            FixedSize::new(
+                Size {
+                    width: 100.0,
+                    height: 50.0,
+                },
+                Color4f::new(1.0, 0.0, 0.0, 1.0),
+            ),
+            FixedSize::new(
+                Size {
+                    width: 100.0,
+                    height: 50.0,
+                },
+                Color4f::new(0.0, 1.0, 0.0, 1.0),
+            ),
+            FixedSize::new(
+                Size {
+                    width: 100.0,
+                    height: 50.0,
+                },
+                Color4f::new(0.0, 0.0, 1.0, 1.0),
+            ),
+        ];
+
+        let context = LayoutContext {};
+        let constraints = BoxConstraints {
+            min_width: 0.0,
+            max_width: f32::infinity(),
+            min_height: 0.0,
+            max_height: f32::infinity(),
+        };
+        let sizes: Vec<Size> = children
+            .iter_mut()
+            .map(|child| child.layout(&context, constraints))
+            .collect();
+
+        let mut own_rect = Rectangle {
+            origin: Point { x: 0.0, y: 0.0 },
+            size: draw_context.size,
+        };
+        for (mut child, size) in children.iter_mut().zip(sizes) {
+            draw_context.canvas.save();
+
+            let child_rect = own_rect.take_left(size.width);
+
+            draw_context
+                .canvas
+                .translate((child_rect.origin.x, child_rect.origin.y));
+            let mut draw_context = DrawContext {
+                size,
+                canvas: draw_context.canvas,
+            };
+            child.draw(&mut draw_context);
+
+            draw_context.canvas.restore();
+        }
+
+        // let canvas = draw_context.canvas;
+        // let paint = Paint::new(Color4f::new(0.0, 1.0, 0.0, 1.0), None);
+        // canvas.draw_circle(
+        //     Point::new(
+        //         draw_context.size.width / 2.0,
+        //         draw_context.size.height / 2.0,
+        //     ),
+        //     100.0,
+        //     &paint,
+        // );
+    }
+}
 
 pub struct LayoutContext {}
 
 pub struct DrawContext<'a> {
     canvas: &'a mut Canvas,
+    size: Size,
 }
 
-pub trait Widget {
-    fn layout(&mut self, layout_context: LayoutContext, box_constraints: BoxConstraints) {}
+impl<'a> DrawContext<'a> {
+    pub fn new(canvas: &'a mut Canvas, draw_size: Size) -> Self {
+        Self {
+            canvas,
+            size: draw_size,
+        }
+    }
+}
 
-    fn draw(&mut self, draw_context: DrawContext) {}
+#[derive(Default, Clone, Copy)]
+pub struct BoxConstraints {
+    min_width: f32,
+    max_width: f32,
+    min_height: f32,
+    max_height: f32,
+}
+
+#[derive(Default, Clone, Copy)]
+pub struct Size {
+    pub width: f32,
+    pub height: f32,
+}
+
+pub trait RenderObject {
+    fn layout(&mut self, layout_context: &LayoutContext, box_constraints: BoxConstraints) -> Size {
+        Size::default()
+    }
+
+    fn draw(&mut self, draw_context: &mut DrawContext) {}
+}
+
+#[derive(Default, Clone, Copy)]
+pub struct Point {
+    x: f32,
+    y: f32,
+}
+
+#[derive(Default, Clone, Copy)]
+pub struct Rectangle {
+    origin: Point,
+    size: Size,
+}
+
+impl Rectangle {
+    pub fn take_left(&mut self, x: f32) -> Rectangle {
+        let origin = self.origin.clone();
+        let mut size = self.size.clone();
+        self.origin.x += x;
+        self.size.width -= x;
+        size.width = x;
+        Rectangle { origin, size }
+    }
+}
+
+struct FixedSize {
+    size: Size,
+    color: Color4f,
+}
+
+impl FixedSize {
+    pub fn new(size: Size, color: Color4f) -> Self {
+        Self { size, color }
+    }
+}
+
+impl RenderObject for FixedSize {
+    fn layout(
+        &mut self,
+        _layout_context: &LayoutContext,
+        _box_constraints: BoxConstraints,
+    ) -> Size {
+        self.size
+    }
+
+    fn draw(&mut self, draw_context: &mut DrawContext) {
+        let paint = Paint::new(self.color, None);
+        draw_context
+            .canvas
+            .draw_rect(skia_safe::Rect::new(0.0, 0.0, 100.0, 100.0), &paint);
+    }
 }
