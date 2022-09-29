5bcbf2dcce7f8aada12a1bed704d123fc0ff4017
yamadapc <yamadapc@users.noreply.github.com>
2022-09-29 11:52:38 +0000

    deploy: 0791c70a891d41bc9883b73e5bfe27660e61eda3

--- daw/flutter_service_worker.js
+++ daw/flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "main.dart.js": "48af70163436be3a5b9d4a3af0dd8015",
-"index.html": "9293300a880c4c652aadd70eaf1ba7f4",
-"/": "9293300a880c4c652aadd70eaf1ba7f4",
+"index.html": "1f96dd05a8080bd69c66911b26fedb5e",
+"/": "1f96dd05a8080bd69c66911b26fedb5e",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "manifest.json": "7f19cd5ab58f66324c3a09b115d1c92d",
 "canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
