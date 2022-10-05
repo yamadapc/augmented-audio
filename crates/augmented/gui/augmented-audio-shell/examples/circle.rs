37e6062068886db19d82f049e20762908eaeb9db
yamadapc <tacla.yamada@gmail.com>
2022-10-06 08:13:55 +1100

    Add some boilerplate for skia

---
+++ crates/augmented/gui/augmented-audio-shell/examples/circle.rs
@@ -0,0 +1,14 @@
+use skia_safe::{Color4f, Paint, Point};
+
+fn main() {
+    augmented_audio_shell::sketch(|ctx| {
+        let size = ctx.size();
+        let canvas = ctx.canvas();
+
+        canvas.clear(Color4f::new(0.0, 0.0, 0.0, 1.0));
+
+        let paint = Paint::new(Color4f::new(0.0, 1.0, 0.0, 1.0), None);
+        let center = Point::new(size.width / 2.0, size.height / 2.0);
+        canvas.draw_circle(center, 100.0, &paint);
+    })
+}
---
+++ crates/augmented/gui/augmented-audio-shell/src/lib.rs
@@ -0,0 +1,139 @@
+use cocoa::{appkit::NSView, base::id};
+use core_graphics_types::geometry::CGSize;
+use foreign_types_shared::{ForeignType, ForeignTypeRef};
+use metal::{Device, MTLPixelFormat, MetalDrawableRef, MetalLayer};
+use objc::{rc::autoreleasepool, runtime::YES};
+use skia_safe::{
+    gpu::mtl::BackendContext,
+    gpu::{mtl, BackendRenderTarget, DirectContext, SurfaceOrigin},
+    scalar, Canvas, ColorType, Size, Surface,
+};
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::ControlFlow,
+    platform::macos::WindowExtMacOS,
+};
+
+pub struct SketchContext<'a> {
+    canvas: &'a mut Canvas,
+    size: Size,
+}
+
+impl<'a> SketchContext<'a> {
+    pub fn canvas(&mut self) -> &mut Canvas {
+        self.canvas
+    }
+
+    pub fn size(&self) -> Size {
+        self.size
+    }
+}
+
+pub fn sketch<B>(mut builder: B)
+where
+    B: FnMut(&mut SketchContext) -> () + 'static,
+{
+    let ev = winit::event_loop::EventLoop::new();
+    let window = winit::window::Window::new(&ev).unwrap();
+
+    let device = Device::system_default().unwrap();
+    let queue = device.new_command_queue();
+    let backend = unsafe {
+        BackendContext::new(
+            device.as_ptr() as mtl::Handle,
+            queue.as_ptr() as mtl::Handle,
+            std::ptr::null(),
+        )
+    };
+    let mut context = DirectContext::new_metal(&backend, None).unwrap();
+
+    let metal_layer = {
+        let draw_size = window.inner_size();
+        let layer = MetalLayer::new();
+        layer.set_device(&device);
+        layer.set_pixel_format(MTLPixelFormat::BGRA8Unorm);
+        layer.set_presents_with_transaction(false);
+
+        unsafe {
+            let view = window.ns_view() as id;
+            view.setWantsLayer(YES);
+            view.setLayer(layer.as_ref() as *const _ as _);
+        }
+
+        layer.set_drawable_size(CGSize::new(draw_size.width as f64, draw_size.height as f64));
+        layer
+    };
+
+    ev.run(move |event, _target, control_flow| {
+        autoreleasepool(|| {
+            *control_flow = ControlFlow::Wait;
+
+            match event {
+                Event::WindowEvent { event, .. } => match event {
+                    WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                    WindowEvent::Resized(size) => {
+                        metal_layer
+                            .set_drawable_size(CGSize::new(size.width as f64, size.height as f64));
+                        window.request_redraw();
+                    }
+                    _ => (),
+                },
+                Event::RedrawRequested(_) => {
+                    get_drawable_surface(&metal_layer, &mut context).map(
+                        |(drawable, mut surface)| {
+                            let canvas = surface.canvas();
+
+                            let mut sketch_context = SketchContext {
+                                canvas,
+                                size: {
+                                    let draw_size = metal_layer.drawable_size();
+                                    Size::new(draw_size.width as scalar, draw_size.height as scalar)
+                                },
+                            };
+                            builder(&mut sketch_context);
+
+                            surface.flush_and_submit();
+                            drop(surface);
+
+                            let command_buffer = queue.new_command_buffer();
+                            command_buffer.present_drawable(drawable);
+                            command_buffer.commit();
+                        },
+                    );
+                }
+                _ => {}
+            }
+        });
+    });
+}
+
+fn get_drawable_surface<'a>(
+    metal_layer: &'a MetalLayer,
+    context: &'a mut DirectContext,
+) -> Option<(&'a MetalDrawableRef, Surface)> {
+    let drawable = metal_layer.next_drawable()?;
+    let drawable_size = {
+        let size = metal_layer.drawable_size();
+        Size::new(size.width as scalar, size.height as scalar)
+    };
+
+    let surface = unsafe {
+        let texture_info = mtl::TextureInfo::new(drawable.texture().as_ptr() as mtl::Handle);
+
+        let backend_render_target = BackendRenderTarget::new_metal(
+            (drawable_size.width as i32, drawable_size.height as i32),
+            1,
+            &texture_info,
+        );
+
+        Surface::from_backend_render_target(
+            context,
+            &backend_render_target,
+            SurfaceOrigin::TopLeft,
+            ColorType::BGRA8888,
+            None,
+            None,
+        )?
+    };
+    Some((drawable, surface))
+}
