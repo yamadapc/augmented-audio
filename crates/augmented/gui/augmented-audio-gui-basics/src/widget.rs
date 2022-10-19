163899bb474bd82ded284de860cc6596f4485470
yamadapc <tacla.yamada@gmail.com>
2022-10-19 20:52:18 +1100

    Add newline

--- crates/augmented/gui/augmented-audio-gui-basics/src/widget.rs
+++ crates/augmented/gui/augmented-audio-gui-basics/src/widget.rs
@@ -20,6 +20,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
+
 use skia_safe::{Canvas, Color4f, Paint, Rect, Size};
 use taffy::prelude::Dimension;
 use taffy::style::{FlexDirection, FlexboxLayout};
