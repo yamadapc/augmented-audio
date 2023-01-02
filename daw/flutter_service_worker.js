fa3fe0d363cacf053b9af1a3a62094e4726660bc
yamadapc <yamadapc@users.noreply.github.com>
2023-01-02 13:42:51 +0000

    deploy: 6637819ef562d0eeee700d80a73260cf45b13e51

--- daw/flutter_service_worker.js
+++ daw/flutter_service_worker.js
@@ -21,8 +21,8 @@ const RESOURCES = {
 "assets/FontManifest.json": "533db2964f00aa0a56d9c03607f21c52",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
 "assets/NOTICES": "f6f06ae2ea0b0e07c3ae38b781c071e0",
-"index.html": "985d8bf0deea2733d832cc810e7d5485",
-"/": "985d8bf0deea2733d832cc810e7d5485",
+"index.html": "3e3068ede85594f04f606795d5819ca3",
+"/": "3e3068ede85594f04f606795d5819ca3",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "900f7b12a5f5670d739f681cdaaaaa11"
 };
