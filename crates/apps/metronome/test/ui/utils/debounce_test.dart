c0610d45d93ee0399a8365a6b83c18c76c48ac75
yamadapc <tacla.yamada@gmail.com>
2023-01-05 09:43:09 +1100

    Add debounce tests to metronome :patch:

---
+++ crates/apps/metronome/test/ui/utils/debounce_test.dart
@@ -0,0 +1,44 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:metronome/ui/utils/debounce.dart';
+
+void main() {
+  test("debounce will flush only once after duration", () async {
+    var counter = 0;
+    var debounced = Debounce(100);
+    for (var i = 0; i < 10; i++) {
+      debounced.run(() {
+        counter++;
+      });
+    }
+    expect(counter, equals(0));
+    await Future.delayed(const Duration(milliseconds: 150));
+    expect(counter, equals(1));
+  });
+
+  test("we can force flush", () {
+    var counter = 0;
+    var debounced = Debounce(100);
+    for (var i = 0; i < 10; i++) {
+      debounced.run(() {
+        counter++;
+      });
+    }
+    expect(counter, equals(0));
+    debounced.flush();
+    expect(counter, equals(1));
+  });
+
+  test("we can cancel pending operations", () {
+    var counter = 0;
+    var debounced = Debounce(100);
+    for (var i = 0; i < 10; i++) {
+      debounced.run(() {
+        counter++;
+      });
+    }
+    expect(counter, equals(0));
+    debounced.cancel();
+    debounced.flush();
+    expect(counter, equals(0));
+  });
+}
