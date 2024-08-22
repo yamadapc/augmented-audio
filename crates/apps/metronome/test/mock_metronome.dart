ffedb9c31d3c78558c5ba6b36e653dbb8b29684f
yamadapc <tacla.yamada@gmail.com>
2024-08-22 17:12:05 +1000

    Fix metronome mocks

--- crates/apps/metronome/test/mock_metronome.dart
+++ crates/apps/metronome/test/mock_metronome.dart
@@ -6,13 +6,17 @@ import 'package:metronome/modules/state/history_state_controller.dart';
 import 'package:metronome/modules/state/history_state_model.dart';
 import 'package:metronome/modules/state/metronome_state_controller.dart';
 import 'package:metronome/modules/state/metronome_state_model.dart';
+import 'package:metronome/src/rust/api.dart';
 import 'package:metronome/src/rust/frb_generated.dart';
+import 'package:metronome/src/rust/internal/processor.dart';
+import 'package:metronome/src/rust/internal/state.dart';
 
 Future<MockEnvironment> buildTestEnvironment() async {
   // Set-up mocked environment
   logger.i("Setting-up mock environment");
   final model = MetronomeStateModel();
-  await RustLib.init();
+  final mockApi = MockRustLibApi();
+  RustLib.initMock(api: mockApi);
   final database = await buildInMemoryDatabase();
   final sessionDao = database.sessionDao;
   final historyStateModel = HistoryStateModel();
@@ -39,6 +43,53 @@ Future<MockEnvironment> buildTestEnvironment() async {
     historyStartStopHandler,
     metronomeStateController,
   );
+}
+
+class MockRustLibApi implements RustLibApi {
+  @override
+  Future<int> crateApiDeinitialize() {
+    return Future.value(0);
+  }
+
+  @override
+  Future<double> crateApiGetPlayhead() {
+    return Future.value(0);
+  }
+
+  @override
+  Future<int> crateApiInitialize({required InitializeOptions options}) {
+    return Future.value(0);
+  }
+
+  @override
+  Future<int> crateApiSetBeatsPerBar({required int value}) {
+    return Future.value(0);
+  }
+
+  @override
+  Future<int> crateApiSetIsPlaying({required bool value}) {
+    return Future.value(0);
+  }
+
+  @override
+  Future<int> crateApiSetSound({required MetronomeSoundTypeTag value}) {
+    return Future.value(0);
+  }
+
+  @override
+  Future<int> crateApiSetTempo({required double value}) {
+    return Future.value(0);
+  }
+
+  @override
+  Future<int> crateApiSetVolume({required double value}) {
+    return Future.value(0);
+  }
+
+  @override
+  Stream<EngineError> crateApiStreamErrors() {
+    return Stream.empty();
+  }
 }
 
 class MockEnvironment {
