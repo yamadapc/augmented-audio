fd636078419f478d99ece24fd7d08f66d36bd61e
yamadapc <tacla.yamada@gmail.com>
2024-08-24 09:33:44 +1000

    Fix little issues with workflow

--- scripts/install-ubuntu-rust-dependencies.sh
+++ scripts/install-ubuntu-rust-dependencies.sh
@@ -1,17 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-if ! [ -x "$(command -v cargo-tarpaulin)" ]; then
-  cargo install cargo-tarpaulin
-fi
-
-if ! [ -x "$(command -v cargo-llvm-cov)" ]; then
-  # Get host target
-  host=$(rustc -Vv | grep host | sed 's/host: //')
-  # Download binary and install to $HOME/.cargo/bin
-  curl -LsSf https://github.com/taiki-e/cargo-llvm-cov/releases/latest/download/cargo-llvm-cov-$host.tar.gz | tar xzf - -C $HOME/.cargo/bin
-fi
-
-#if ! [ -x "$(command -v cargo-nextest)" ]; then
-#  curl -LsSf https://get.nexte.st/latest/linux | tar zxf - -C ${CARGO_HOME:-~/.cargo}/bin
-#fi
+curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+cargo binstall cargo-tarpaulin
+cargo binstall cargo-llvm-cov
+cargo binstall cargo-nextest
