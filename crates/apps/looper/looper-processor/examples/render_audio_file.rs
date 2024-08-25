05e4a6a7ff148ce10882060c8b57052a59d9efe6
yamadapc <tacla.yamada@gmail.com>
2024-08-25 22:07:52 +1000

    Fix more broken linux bits

--- crates/apps/looper/looper-processor/examples/render_audio_file.rs
+++ crates/apps/looper/looper-processor/examples/render_audio_file.rs
@@ -21,58 +21,72 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-use cacao::appkit::window::Window;
-use cacao::appkit::{App, AppDelegate};
-use cacao::view::View;
+#[cfg(target_os = "macos")]
+mod implementation {
+    use cacao::appkit::window::Window;
+    use cacao::appkit::{App, AppDelegate};
+    use cacao::view::View;
 
-use audio_processor_file::AudioFileProcessor;
-use audio_processor_traits::{AudioBuffer, AudioContext, AudioProcessor};
+    use audio_processor_file::AudioFileProcessor;
+    use audio_processor_traits::{AudioBuffer, AudioContext, AudioProcessor};
 
-#[allow(unused)]
-fn read_test_buffer() -> AudioBuffer<f32> {
-    let input = audio_processor_testing_helpers::relative_path!("../../../../input-files/bass.mp3");
-    let input = std::path::Path::new(&input).canonicalize().unwrap();
+    #[allow(unused)]
+    fn read_test_buffer() -> AudioBuffer<f32> {
+        let input =
+            audio_processor_testing_helpers::relative_path!("../../../../input-files/bass.mp3");
+        let input = std::path::Path::new(&input).canonicalize().unwrap();
 
-    let mut input_file = AudioFileProcessor::from_path(
-        audio_garbage_collector::handle(),
-        Default::default(),
-        input.to_str().unwrap(),
-    )
-    .unwrap();
+        let mut input_file = AudioFileProcessor::from_path(
+            audio_garbage_collector::handle(),
+            Default::default(),
+            input.to_str().unwrap(),
+        )
+        .unwrap();
 
-    let mut context = AudioContext::default();
-    input_file.prepare(&mut context);
-    let input_file = input_file.buffer();
+        let mut context = AudioContext::default();
+        input_file.prepare(&mut context);
+        let input_file = input_file.buffer();
 
-    let mut buffer = AudioBuffer::empty();
-    buffer.resize(input_file.len(), input_file[0].len());
-    for (c, channel) in input_file.iter().enumerate() {
-        for (s, sample) in channel.iter().enumerate() {
-            buffer.set(c, s, *sample);
+        let mut buffer = AudioBuffer::empty();
+        buffer.resize(input_file.len(), input_file[0].len());
+        for (c, channel) in input_file.iter().enumerate() {
+            for (s, sample) in channel.iter().enumerate() {
+                buffer.set(c, s, *sample);
+            }
+        }
+        buffer
+    }
+
+    #[derive(Default)]
+    struct RenderAudioFileApp {
+        window: Window,
+        content_view: View,
+    }
+
+    impl AppDelegate for RenderAudioFileApp {
+        fn did_finish_launching(&self) {
+            self.window.set_minimum_content_size(400., 400.);
+            self.window.set_title("Render audio file");
+            self.window.show();
+
+            self.content_view
+                .set_background_color(cacao::color::Color::SystemPink);
+            self.window.set_content_view(&self.content_view)
         }
     }
-    buffer
-}
 
-#[derive(Default)]
-struct RenderAudioFileApp {
-    window: Window,
-    content_view: View,
-}
-
-impl AppDelegate for RenderAudioFileApp {
-    fn did_finish_launching(&self) {
-        self.window.set_minimum_content_size(400., 400.);
-        self.window.set_title("Render audio file");
-        self.window.show();
-
-        self.content_view
-            .set_background_color(cacao::color::Color::SystemPink);
-        self.window.set_content_view(&self.content_view)
+    pub fn main() {
+        wisual_logger::init_from_env();
+        App::new("com.beijaflor.loadaudio", RenderAudioFileApp::default()).run();
     }
 }
 
+#[cfg(target_os = "macos")]
 fn main() {
-    wisual_logger::init_from_env();
-    App::new("com.beijaflor.loadaudio", RenderAudioFileApp::default()).run();
+    implementation::main();
+}
+
+#[cfg(not(target_os = "macos"))]
+fn main() {
+    todo!()
 }
